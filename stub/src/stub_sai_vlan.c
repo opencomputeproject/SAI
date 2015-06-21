0c31665d0b26c47b1bbbbecb36a084c98b1fa148
Soramichi Akiyama <akiyama@m.soramichi.jp>
2015-06-22 00:27:26 +0900

    add storage layer to stub_sai_vlan.c
    As a first trial step of contributing storage layer codes to sai/stub discussed here,
    this pull request enables useful error messaging by comparing given API parameters
    and the VLAN states already created by former API calls.
    
    For example, this patch allows stub_create_vlan to check if the given VLAN id is already in use,
    by adding the storge layer code to store states.
    
    Example: if vlan_api->create_vlan(10) is called twice this patch yields a warning:
    
    [SAI_VLAN.WARNING] stub_sai_vlan.c[178]- stub_create_vlan: Warning: given vlan_id (10) already exsits.

--- stub/src/stub_sai_vlan.c
+++ stub/src/stub_sai_vlan.c
@@ -21,13 +21,30 @@
 #undef  __MODULE__
 #define __MODULE__ SAI_VLAN
 
+#define vlan_id_range_ok(vlan_id) ((vlan_id)>=1 && (vlan_id)<=4095)
+
+
+/* Storage layer data structures / variables to store the states */
+static int number_of_vlans;
+struct __vlan* vlans = NULL;
+
+struct __vlan {
+    sai_vlan_id_t id;
+    int number_of_ports;
+    sai_vlan_port_t* port_list;
+};
+
+
 static const sai_attribute_entry_t vlan_attribs[] = {
-    { SAI_VLAN_ATTR_MAX_LEARNED_ADDRESSES, false, false, true, true,
-      "Vlan Maximum number of learned MAC addresses", SAI_ATTR_VAL_TYPE_U32 },
-    { SAI_VLAN_ATTR_STP_INSTANCE, false, false, true, true,
-      "Vlan associated STP instance", SAI_ATTR_VAL_TYPE_U64 },
-    { END_FUNCTIONALITY_ATTRIBS_ID, false, false, false, false,
-      "", SAI_ATTR_VAL_TYPE_UNDETERMINED }
+    {   SAI_VLAN_ATTR_MAX_LEARNED_ADDRESSES, false, false, true, true,
+        "Vlan Maximum number of learned MAC addresses", SAI_ATTR_VAL_TYPE_U32
+    },
+    {   SAI_VLAN_ATTR_STP_INSTANCE, false, false, true, true,
+        "Vlan associated STP instance", SAI_ATTR_VAL_TYPE_U64
+    },
+    {   END_FUNCTIONALITY_ATTRIBS_ID, false, false, false, false,
+        "", SAI_ATTR_VAL_TYPE_UNDETERMINED
+    }
 };
 
 sai_status_t stub_vlan_max_learned_addr_get(_In_ const sai_object_key_t   *key,
@@ -48,16 +65,18 @@ sai_status_t stub_vlan_stp_set(_In_ const sai_object_key_t      *key,
                                void                             *arg);
 
 static const sai_vendor_attribute_entry_t vlan_vendor_attribs[] = {
-    { SAI_VLAN_ATTR_MAX_LEARNED_ADDRESSES,
-      { false, false, true, true },
-      { false, false, true, true },
-      stub_vlan_max_learned_addr_get, NULL,
-      stub_vlan_max_learned_addr_set, NULL },
-    { SAI_VLAN_ATTR_STP_INSTANCE,
-      { false, false, true, true },
-      { false, false, true, true },
-      stub_vlan_stp_get, NULL,
-      stub_vlan_stp_set, NULL },
+    {   SAI_VLAN_ATTR_MAX_LEARNED_ADDRESSES,
+        { false, false, true, true },
+        { false, false, true, true },
+        stub_vlan_max_learned_addr_get, NULL,
+        stub_vlan_max_learned_addr_set, NULL
+    },
+    {   SAI_VLAN_ATTR_STP_INSTANCE,
+        { false, false, true, true },
+        { false, false, true, true },
+        stub_vlan_stp_get, NULL,
+        stub_vlan_stp_set, NULL
+    },
 };
 static void vlan_key_to_str(_In_ sai_vlan_id_t vlan_id, _Out_ char *key_str)
 {
@@ -148,9 +167,38 @@ sai_status_t stub_remove_all_vlans(void)
 sai_status_t stub_create_vlan(_In_ sai_vlan_id_t vlan_id)
 {
     char key_str[MAX_KEY_STR_LEN];
+    int i;
 
     vlan_key_to_str(vlan_id, key_str);
     STUB_LOG_NTC("Create vlan %s\n", key_str);
+
+    // make sure the given vlan_id satisfies the spec
+    if (!vlan_id_range_ok(vlan_id)) {
+        STUB_LOG_ERR("vlan_id (%d) must satisfy 1 <= vlan_id <= 4094\n", vlan_id);
+        return SAI_STATUS_INVALID_VLAN_ID;
+    }
+
+    // make sure the given vlan_id is available
+    for (i = 0; i < number_of_vlans; i++) {
+        if (vlans[i].id == vlan_id) {
+            STUB_LOG_WRN("Warning: given vlan_id (%d) already exsits.\n", vlan_id);
+            return SAI_STATUS_INVALID_VLAN_ID;
+        }
+    }
+
+    // add new vlan
+    number_of_vlans++;
+    vlans = realloc(vlans, number_of_vlans * sizeof(struct __vlan));
+
+    if (vlans == NULL) {
+        STUB_LOG_ERR("Error: memory allocation for creating a new vlan failed.\n");
+        return SAI_STATUS_NO_MEMORY;
+    }
+
+    struct __vlan* v = &(vlans[number_of_vlans - 1]);
+    v->id = vlan_id;
+    v->number_of_ports = 0;
+    v->port_list = NULL;
 
     return SAI_STATUS_SUCCESS;
 }
@@ -170,8 +218,36 @@ sai_status_t stub_create_vlan(_In_ sai_vlan_id_t vlan_id)
 sai_status_t stub_remove_vlan(_In_ sai_vlan_id_t vlan_id)
 {
     char key_str[MAX_KEY_STR_LEN];
+    int i, index_removed_vlan = -1;
 
     vlan_key_to_str(vlan_id, key_str);
+
+    // make sure the given vlan_id exists
+    for (i = 0; i < number_of_vlans; i++) {
+        if (vlans[i].id == vlan_id) {
+            index_removed_vlan = i;
+            break;
+        }
+    }
+    if (index_removed_vlan == -1) {
+        STUB_LOG_NTC("the given vlan id (%d) does not exist.\n", vlan_id);
+        return SAI_STATUS_INVALID_VLAN_ID;
+    }
+
+    // delete the vlans[index_removed_vlan]
+    for (i = 0; i < number_of_vlans; i++) {
+        if (i > index_removed_vlan) {
+            vlans[i - 1] = vlans[i];
+        }
+    }
+    number_of_vlans--;
+    vlans = realloc(vlans, sizeof(struct __vlan) * number_of_vlans);
+    // Note: realloc(ptr, 0) returns NULL, which is not an error
+    if (vlans == NULL && number_of_vlans > 0) {
+        STUB_LOG_ERR("Error: memory allocation for removing a vlan failed.\n");
+        return SAI_STATUS_NO_MEMORY;
+    }
+
     STUB_LOG_NTC("Remove vlan %s\n", key_str);
 
     return SAI_STATUS_SUCCESS;
@@ -195,9 +271,62 @@ sai_status_t stub_add_ports_to_vlan(_In_ sai_vlan_id_t          vlan_id,
                                     _In_ const sai_vlan_port_t* port_list)
 {
     STUB_LOG_ENTER();
+
+    char key_str[MAX_KEY_STR_LEN];
+    int i, index_target_vlan = -1;
+
+    vlan_key_to_str(vlan_id, key_str);
+
+    for (i = 0; i < number_of_vlans; i++) {
+        if (vlans[i].id == vlan_id) {
+            index_target_vlan = i;
+            break;
+        }
+    }
+
+    if (index_target_vlan == -1) {
+        STUB_LOG_WRN("the given vlan id (%d) does not exist.\n", vlan_id);
+        return SAI_STATUS_INVALID_VLAN_ID;
+    }
+
+    struct __vlan* v = &vlans[index_target_vlan];
+    int old_size = v->number_of_ports, new_size = old_size + port_count;
+    v->port_list = realloc(v->port_list, new_size * sizeof(sai_vlan_port_t));
+
+    if (v->port_list == NULL) {
+        STUB_LOG_ERR("Error: memory allocation for removing a vlan failed.\n");
+        return SAI_STATUS_NO_MEMORY;
+    }
+
+    v->number_of_ports = new_size;
+    memcpy(v->port_list + old_size, port_list, port_count * sizeof(sai_vlan_port_t));
+
     return SAI_STATUS_SUCCESS;
 }
 
+static int remove_a_port_from_vlan(struct __vlan* v, sai_vlan_port_t port)
+{
+    int i, j;
+    int deleted = 0;
+
+    for (i = 0; i < v->number_of_ports; i++) {
+        if (port.port_id == v->port_list[i].port_id) {
+            // delete this port
+            deleted = 1;
+
+            for (j = i + 1; j < v->number_of_ports; j++) {
+                v->port_list[j - 1] = v->port_list[j];
+            }
+
+            (v->number_of_ports)--;
+            v->port_list = realloc(v->port_list, sizeof(sai_vlan_port_t) * v->number_of_ports);
+
+            break;
+        }
+    }
+
+    return deleted;
+}
 
 /*
  * Routine Description:
@@ -217,6 +346,32 @@ sai_status_t stub_remove_ports_from_vlan(_In_ sai_vlan_id_t          vlan_id,
                                          _In_ const sai_vlan_port_t* port_list)
 {
     STUB_LOG_ENTER();
+
+    char key_str[MAX_KEY_STR_LEN];
+    struct __vlan* v = NULL;
+    int i;
+
+    vlan_key_to_str(vlan_id, key_str);
+
+    for (i = 0; i < number_of_vlans; i++) {
+        if (vlans[i].id == vlan_id) {
+            v = &vlans[i];
+            break;
+        }
+    }
+
+    if (v == NULL) {
+        STUB_LOG_WRN("the given vlan id (%d) does not exist.\n", vlan_id);
+        return SAI_STATUS_INVALID_VLAN_ID;
+    }
+
+    for (i = 0; i < (int)port_count; i++) {
+        int ret = remove_a_port_from_vlan(v, port_list[i]);
+        if (ret == 0) {
+            STUB_LOG_NTC("the given port (%d) does not belong to the given vlan (%d)\n", port_list[i].port_id, vlan_id);
+        }
+    }
+
     return SAI_STATUS_SUCCESS;
 }
 
