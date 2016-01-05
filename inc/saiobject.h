5e187360dd227a67ebef1671f67b57d6ef5cc246
Anbalagan Natchimuthu <anbalagan_natchimuth@dell.com>
2016-01-04 21:04:00 -0800

    Modified header in saiobject.h

--- inc/saiobject.h
+++ inc/saiobject.h
@@ -1,31 +1,22 @@
 /*
- * Copyright (c) 2014 Microsoft Open Technologies, Inc.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License"); you may
- *    not use this file except in compliance with the License. You may obtain
- *    a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- *
- *    THIS CODE IS PROVIDED ON AN  *AS IS* BASIS, WITHOUT WARRANTIES OR
- *    CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT
- *    LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS
- *    FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT.
- *
- *    See the Apache Version 2.0 License for specific language governing
- *    permissions and limitations under the License.
- *
- *    Microsoft would like to thank the following companies for their review and
- *    assistance with these files: Intel Corporation, Mellanox Technologies Ltd,
- *    Dell Products, L.P., Facebook, Inc
- *
- * Module Name:
- *
- *    saiobject.h
- *
- * Abstract:
- *
- *    This module defines SAI APIs for bulk retrieval for each object-type
- *
- */
+ *  Copyright (c) 2016 Dell Inc.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may
+ *  not use this file except in compliance with the License. You may obtain
+ *  a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  THIS CODE IS PROVIDED ON AN  *AS IS* BASIS, WITHOUT WARRANTIES OR
+ *  CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT
+ *  LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS
+ *  FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT.
+ * 
+ *  See the Apache Version 2.0 License for specific language governing
+ *  permissions and limitations under the License.
+ * 
+ *  @file saiobject.h
+ * 
+ *  @brief This module defines SAI APIs for bulk retrieval for each object-type 
+ *************************************************************************/
 
 #if !defined (__SAIOBJECT_H_)
 #define __SAIOBJECT_H_
