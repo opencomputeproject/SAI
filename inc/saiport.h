6f4be99a0cef7d5f53db5a4f901158cd0172ef18
Rajasekhar Pulluru <Rajasekhar_Pulluru@dell.com>
2016-04-21 07:22:48 -0700

    create an enum type for transceivers and moved qsfp/sfp enum from media type enum to transceiver enum type removed full-duplex supported/advertised attribute as supported/advertised speed is full-duplex added a new attribute to query supported/advertised ASYMMETRIC Pause
    Changes to be committed:
    	modified:   inc/saiport.h

--- inc/saiport.h
+++ inc/saiport.h
@@ -184,25 +184,37 @@ typedef enum _sai_port_media_type_t
 
     /** Media type copper */
     SAI_PORT_MEDIA_TYPE_COPPER,
-
-    /** Media type QSFP28 fiber optic */
-    SAI_PORT_MEDIA_TYPE_QSFP28_FIBER,
-
-    /** Media type QSFP28 copper */
-    SAI_PORT_MEDIA_TYPE_QSFP28_COPPER,
-
-    /** Media type QSFP fiber optic */
-    SAI_PORT_MEDIA_TYPE_QSFP_FIBER,
-
-    /** Media type QSFP copper optic */
-    SAI_PORT_MEDIA_TYPE_QSFP_COPPER,
-
-    /** Media type SFP fiber optic */
-    SAI_PORT_MEDIA_TYPE_SFP_FIBER,
-
-    /** Media type SFP copper optic */
-    SAI_PORT_MEDIA_TYPE_SFP_COPPER,
 } sai_port_media_type_t;
+
+/** 
+ * @brief Attribute data for SAI_PORT_ATTR_TRANSCEIVER_TYPE
+ */
+typedef enum _sai_port_transceiver_type_t
+{
+    /** Transceiver not present */
+    SAI_PORT_TRANSCEIVER_TYPE_NOT_PRESENT,
+
+    /** Transceiver type not known */
+    SAI_PORT_TRANSCEIVER_TYPE_UNKNOWN,
+
+    /** Transceiver type QSFP28 fiber optic */
+    SAI_PORT_TRANSCEIVER_TYPE_QSFP28_FIBER,
+
+    /** Transceiver type QSFP28 copper */
+    SAI_PORT_TRANSCEIVER_TYPE_QSFP28_COPPER,
+
+    /** Transceiver type QSFP fiber optic */
+    SAI_PORT_TRANSCEIVER_TYPE_QSFP_FIBER,
+
+    /** Transceiver type QSFP copper optic */
+    SAI_PORT_TRANSCEIVER_TYPE_QSFP_COPPER,
+
+    /** Transceiver type SFP fiber optic */
+    SAI_PORT_TRANSCEIVER_TYPE_SFP_FIBER,
+
+    /** Transceiver type SFP copper optic */
+    SAI_PORT_TRANSCEIVER_TYPE_SFP_COPPER,
+} sai_port_transceiver_type_t;
 
 /**
  *  Attribute Id in sai_set_port_attribute() and
@@ -239,11 +251,9 @@ typedef enum _sai_port_attr_t
     /** List of Scheduler groups for the port[sai_object_list_t] */
     SAI_PORT_ATTR_QOS_SCHEDULER_GROUP_LIST,
 
-    /** Query list of supported port speed in Mbps [sai_u32_list_t] */
+    /** Query list of supported port speed (Full-Duplex) 
+      * in Mbps [sai_u32_list_t] */
     SAI_PORT_ATTR_SUPPORTED_SPEED,
-
-    /** Query list of Supported Full-Duplex speed in Mbps [sai_u32_list_t] */
-    SAI_PORT_ATTR_SUPPORTED_FULL_DUPLEX_SPEED,
 
     /** Query list of Supported HALF-Duplex speed in Mbps [sai_u32_list_t] */
     SAI_PORT_ATTR_SUPPORTED_HALF_DUPLEX_SPEED,
@@ -254,15 +264,15 @@ typedef enum _sai_port_attr_t
     /** Query port supported flow control mode [sai_port_flow_control_mode_t] */
     SAI_PORT_ATTR_SUPPORTED_FLOW_CONTROL,
 
+    /** Query port supported asymmetric pause mode [bool] */
+    SAI_PORT_ATTR_SUPPORTED_ASYMMETRIC_PAUSE_MODE,
+
     /** Query port supported MEDIA type [sai_port_media_type_t] */
     SAI_PORT_ATTR_SUPPORTED_MEDIA_TYPE,
 
-    /** Query list of supported remote port speed in Mbps [sai_u32_list_t] */
+    /** Query list of supported remote port speed (Full-Duplex) 
+      * in Mbps [sai_u32_list_t] */
     SAI_PORT_ATTR_REMOTE_SUPPORTED_SPEED,
-
-    /** Query list of Remote port’s Supported
-      * Full-Duplex speed in Mbps [sai_u32_list_t] */
-    SAI_PORT_ATTR_REMOTE_SUPPORTED_FULL_DUPLEX_SPEED,
 
     /** Query list of Remote Port’s Supported
       * HALF-Duplex speed in Mbps [sai_u32_list_t] */
@@ -275,15 +285,15 @@ typedef enum _sai_port_attr_t
       * [sai_port_flow_control_mode_t] */
     SAI_PORT_ATTR_REMOTE_SUPPORTED_FLOW_CONTROL,
 
+    /** Query Remote port supported asymmetric pause mode [bool] */
+    SAI_PORT_ATTR_REMOTE_SUPPORTED_ASYMMETRIC_PAUSE_MODE,
+
     /** Query Remote port MEDIA type [sai_port_media_type_t] */
     SAI_PORT_ATTR_REMOTE_SUPPORTED_MEDIA_TYPE,
 
-    /** Query list of Advertised remote port speed in Mbps [sai_u32_list_t] */
+    /** Query list of Advertised remote port speed (Full-Duplex)
+      * in Mbps [sai_u32_list_t] */
     SAI_PORT_ATTR_REMOTE_ADVERTISED_SPEED,
-
-    /** Query list of Remote port’s Advertised
-      * Full-Duplex speed in Mbps [sai_u32_list_t] */
-    SAI_PORT_ATTR_REMOTE_ADVERTISED_FULL_DUPLEX_SPEED,
 
     /** Query list of Remote Port’s
       * Advertised HALF-Duplex speed in Mbps [sai_u32_list_t] */
@@ -295,6 +305,9 @@ typedef enum _sai_port_attr_t
     /** Query Remote port Advertised flow control mode
       * [sai_port_flow_control_mode_t] */
     SAI_PORT_ATTR_REMOTE_ADVERTISED_FLOW_CONTROL,
+
+    /** Query Remote port Advertised asymmetric pause mode [bool] */
+    SAI_PORT_ATTR_REMOTE_ADVERTISED_ASYMMETRIC_PAUSE_MODE,
 
     /** Query Remote port Advertised MEDIA type [sai_port_media_type_t] */
     SAI_PORT_ATTR_REMOTE_ADVERTISED_MEDIA_TYPE,
@@ -322,12 +335,12 @@ typedef enum _sai_port_attr_t
      * (default to SAI_PORT_MEDIA_TYPE_NOT_PRESENT) */
     SAI_PORT_ATTR_MEDIA_TYPE,
 
-    /** Query/Configure list of Advertised port speed in Mbps [sai_u32_list_t] */
-    SAI_PORT_ATTR_ADVERTISED_SPEED,
+    /** Transceiver Type [sai_port_transceiver_type_t] */
+    SAI_PORT_ATTR_TRANSCEIVER_TYPE,
 
-    /** Query/Configure list of Advertised
-     * Full-Duplex speed in Mbps [sai_u32_list_t] */
-    SAI_PORT_ATTR_ADVERTISED_FULL_DUPLEX_SPEED,
+    /** Query/Configure list of Advertised port speed (Full-Duplex)
+      * in Mbps [sai_u32_list_t] */
+    SAI_PORT_ATTR_ADVERTISED_SPEED,
 
     /** Query/Configure list of Advertised
      * HALF-Duplex speed in Mbps [sai_u32_list_t] */
@@ -339,6 +352,9 @@ typedef enum _sai_port_attr_t
     /** Query/Configure Port’s Advertised flow control mode
       * [sai_port_flow_control_mode_t] */
     SAI_PORT_ATTR_ADVERTISED_FLOW_CONTROL,
+
+    /** Query port's Advertised asymmetric pause mode [bool] */
+    SAI_PORT_ATTR_ADVERTISED_ASYMMETRIC_PAUSE_MODE,
 
     /** Query/Configure Port’s Advertised media type [sai_port_media_type_t] */
     SAI_PORT_ATTR_ADVERTISED_MEDIA_TYPE,
