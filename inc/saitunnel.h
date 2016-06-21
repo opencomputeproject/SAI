4c08cbfcd233823202a69598902f56b67d2df634
Zhichen Ren <zhichenr@mellanox.com>
2016-06-21 11:42:13 -0700

    Remove decap ttl val and decap dscp val
    For uniform mode, ttl val and dscp val are copied from outer header in
    decap.
    For pipe mode, ttl val and dscp val are copied from innner header in
    decap.
    Thus ttl val and dscp val are not used for either mode.

--- inc/saitunnel.h
+++ inc/saitunnel.h
@@ -271,19 +271,11 @@ typedef enum _sai_tunnel_attr_t
      *  Default would be SAI_TUNNEL_TTL_UNIFORM_MODEL */
     SAI_TUNNEL_ATTR_DECAP_TTL_MODE,
 
-    /** tunnel TTL value [sai_uint8_t]
-     *  (valid and MANDATORY_ON_CREATE when SAI_TUNNEL_DECAP_TTL_MODE = SAI_TUNNEL_TTL_PIPE_MODEL) */
-    SAI_TUNNEL_ATTR_DECAP_TTL_VAL,
-
     /** tunnel dscp mode (pipe or uniform model) [sai_tunnel_dscp_mode_t]
      *  (MANDATORY_ON_CREATE when SAI_TUNNEL_ATTR_TYPE=SAI_TUNNEL_IPINIP,SAI_TUNNEL_IPINIP_GRE)
      *  (CREATE_ONLY)
      *  Default would be SAI_TUNNEL_DSCP_UNIFORM_MODEL */
     SAI_TUNNEL_ATTR_DECAP_DSCP_MODE,
-
-    /** tunnel DSCP value [sai_uint8_t : 6]
-     *  (valid and MANDATORY_ON_CREATE when SAI_TUNNEL_DECAP_DSCP_MODE = SAI_TUNNEL_DSCP_PIPE_MODEL) */
-    SAI_TUNNEL_ATTR_DECAP_DSCP_VAL,
 
     /** Custom range base value */
     SAI_TUNNEL_ATTR_CUSTOM_RANGE_BASE = 0x10000000
