9506a311bef4f9e6b57dcbb9630776b12417e82a
Iurii <Iurii.Gordynskyi@cavium.com>
2016-09-30 18:32:07 +0300

    Fixed passed attribute in sai_thrift_create_virtual_router() routine (#215) (#243)
    Signed-off-by: Iurii.Gordynskyi <Iurii.Gordynskyi@cavium.com>

--- test/saithrift/tests/switch.py
+++ test/saithrift/tests/switch.py
@@ -128,7 +128,7 @@ def sai_thrift_create_virtual_router(client, v4_enabled, v6_enabled):
     #v6 enabled
     vr_attribute2_value = sai_thrift_attribute_value_t(booldata=v6_enabled)
     vr_attribute2 = sai_thrift_attribute_t(id=SAI_VIRTUAL_ROUTER_ATTR_ADMIN_V6_STATE,
-                                           value=vr_attribute1_value)
+                                           value=vr_attribute2_value)
     vr_attr_list = [vr_attribute1, vr_attribute2]
     vr_id = client.sai_thrift_create_virtual_router(thrift_attr_list=vr_attr_list)
     return vr_id
