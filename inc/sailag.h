5ccdadbf343baec775822c4c5598c09cf7b7c2c2
Zubin Shah <zubin.shah@cavium.com>
2016-12-08 08:49:19 -0800

    ACL model changes for SAI 1.0.0 (#171)
    * initial version of ACL model based on Community discussion 08/08/2016
    
    Signed-off-by: Zubin Shah <zubin.shah@caviumnetworks.com>
    
    *  updated acl model with header changes - phase 1 (initial version)
    
    Signed-off-by: Zubin Shah <zubin.shah@caviumnetworks.com>
    
    * updated acl model proposal for metadata info
    
    Signed-off-by: Zubin Shah <zubin.shah@caviumnetworks.com>
    
    * update acl model, with more details
    
    Signed-off-by: Zubin Shah <zubin.shah@caviumnetworks.com>
    
    * updated pipeline model for acls
    
    Signed-off-by: Zubin Shah <zubin.shah@caviumnetworks.com>
    
    *  updated sai acl model proposal for sai 1.0.0
    
    Signed-off-by: Zubin Shah <zubin.shah@caviumnetworks.com>
    
    * updated saiacl table group - apis and attributes
    
    Signed-off-by: Zubin Shah <zubin.shah@caviumnetworks.com>
    
    * updated examples in the SAI ACL model proposal
    
    Signed-off-by: Zubin Shah <zubin.shah@caviumnetworks.com>
    
    *  clarify multiple bind points across ACL tables
    
    Signed-off-by: Zubin Shah <zubin.shah@caviumnetworks.com>
    
    *  clarify multiple bind points across ACL tables
    
    Signed-off-by: Zubin Shah <zubin.shah@caviumnetworks.com>
    
    *  update read_only attribute to query non-conflicting action resolution across ACL groups
    
    Signed-off-by: Zubin Shah <zubin.shah@caviumnetworks.com>
    
    * updated behavioral model and pipeline model with ACL changes for .1d, .1q, router flow, and multicast flow for review
    
    Signed-off-by: Zubin Shah <zubin.shah@caviumnetworks.com>
    
    * fix some formatting issue on the proposal doc
    
    Signed-off-by: Zubin Shah <zubin.shah@caviumnetworks.com>
    
    * updated formatting and comments
    
    Signed-off-by: Zubin Shah <zubin.shah@caviumnetworks.com>
    
    *  updated review comments from 10/13/2016
    
    Signed-off-by: Zubin Shah <zubin.shah@caviumnetworks.com>
    
    * updated spec and headers (1) tunnel behavior (2) ++ bind point as MANDATORY_ON_CREATE for acl table/groups
    
    Signed-off-by: Zubin Shah <zubin.shah@caviumnetworks.com>
    
    * updated spec with minor comments
    
    Signed-off-by: Zubin Shah <zubin.shah@caviumnetworks.com>
    
    * fix sanity failures in ACL Model specification and header files
    
    Signed-off-by: Zubin <zubin.shah@caviumnetworks.com>
    
    * update acl model comments on nov9th
    
    Signed-off-by: Zubin Shah <zubin.shah@caviumnetworks.com>
    
    * fix all meta checkers
    
    Signed-off-by: Zubin <zubin.shah@caviumnetworks.com>
    
    * update acl model pipeline diagram
    
    Signed-off-by: Zubin Shah <zubin.shah@caviumnetworks.com>
    
    * update proposal based on header changes - relevant to meta fixes
    
    Signed-off-by: Zubin Shah <zubin.shah@caviumnetworks.com>
    
    *  added group attribute - parallel and sequential
    
    Signed-off-by: Zubin <zubin.shah@caviumnetworks.com>
    
    * ++ ACL group member object, group member management APIs and attributes
    
    Signed-off-by: Zubin <zubin.shah@caviumnetworks.com>
    
    * updates to the proposal/spec - as reviewed on 11/17/2016
    
    Signed-off-by: Zubin Shah <zubin.shah@caviumnetworks.com>
    
    * remove <br> tags from the document
    
    Signed-off-by: Zubin <zubin.shah@caviumnetworks.com>
    
    * updated proposal to reflect new header changes, updated examples, review comments
    
    Signed-off-by: Zubin <zubin.shah@caviumnetworks.com>
    
    *  updated dell's comments on (1) switch attribute renaming (2) mandatory on create for group id in member(s)
    
    Signed-off-by: Zubin Shah <zubin.shah@caviumnetworks.com>
    
    * fixed meta compilation
    
    Signed-off-by: Zubin Shah <zubin.shah@caviumnetworks.com>

--- inc/sailag.h
+++ inc/sailag.h
@@ -52,6 +52,38 @@ typedef enum _sai_lag_attr_t
      */
     SAI_LAG_ATTR_PORT_LIST = SAI_LAG_ATTR_START,
 
+    /** READ_WRITE */
+
+    /** 
+     * @brief LAG bind point for ingress ACL object
+     *
+     * Bind (or unbind) an ingress acl table or acl group on a LAG. Enable/Update 
+     * ingress ACL table or ACL group filtering by assigning the list of valid 
+     * object id. Disable ingress filtering by assigning SAI_NULL_OBJECT_ID 
+     * in the attribute value. 
+     *
+     * @type sai_object_id_t
+     * @objects SAI_OBJECT_TYPE_ACL_TABLE, SAI_OBJECT_TYPE_ACL_TABLE_GROUP
+     * @flags CREATE_AND_SET
+     * @default SAI_NULL_OBJECT_ID
+     */
+    SAI_LAG_ATTR_INGRESS_ACL,
+
+    /** 
+     * @brief LAG bind point for egress ACL object
+     *
+     * Bind (or unbind) an egress acl tables or acl groups on a LAG. Enable/Update 
+     * egress ACL table or ACL group filtering by assigning the list of valid 
+     * object id. Disable egress filtering by assigning SAI_NULL_OBJECT_ID 
+     * in the attribute value. 
+     *
+     * @type sai_object_id_t
+     * @objects SAI_OBJECT_TYPE_ACL_TABLE, SAI_OBJECT_TYPE_ACL_TABLE_GROUP
+     * @flags CREATE_AND_SET
+     * @default SAI_NULL_OBJECT_ID
+     */
+    SAI_LAG_ATTR_EGRESS_ACL,
+
     /**
      * @brief End of attributes
      */
