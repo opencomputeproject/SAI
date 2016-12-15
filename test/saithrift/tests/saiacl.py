b8724ade22b7a6bfedd449761ddf1231d61023e0
Iurii Gordynskyi <Iurii.Gordynskyi@cavium.com>
2016-12-15 15:55:46 +0200

    Added "saiacl.SeqAclTableGroupTest" test case according to new SAI ACL model
    Signed-off-by: Iurii Gordynskyi <Iurii.Gordynskyi@cavium.com>

--- test/saithrift/tests/saiacl.py
+++ test/saithrift/tests/saiacl.py
@@ -201,7 +201,7 @@ class MACSrcAclTest(sai_base_test.ThriftInterfaceDataPlane):
 
         print "Sending packet ptf_intf 2 -[acl]-> ptf_intf 1 (192.168.0.1 -[acl]-> 10.10.10.1 [id = 105])"
         print 'ACL \'DROP, src mac 00:22:22:22:22:22, in_ports[ptf_intf_1,2]\' Applied '
-        # setup ACL to block based on Source IP
+        # setup ACL to block based on Source MAC
         table_stage = SAI_ACL_STAGE_INGRESS
         table_bind_point_list = [SAI_ACL_BIND_POINT_TYPE_PORT]
         entry_priority = 1
@@ -409,6 +409,205 @@ class L3AclTest(sai_base_test.ThriftInterfaceDataPlane):
             # cleanup ACL
             self.client.sai_thrift_remove_acl_entry(acl_entry_id)
             self.client.sai_thrift_remove_acl_table(acl_table_id)
+            # cleanup
+            sai_thrift_remove_route(self.client, vr_id, addr_family, ip_addr1, ip_mask1, rif_id1)
+            self.client.sai_thrift_remove_next_hop(nhop1)
+            sai_thrift_remove_neighbor(self.client, addr_family, rif_id1, ip_addr1, dmac1)
+            self.client.sai_thrift_remove_router_interface(rif_id1)
+            self.client.sai_thrift_remove_router_interface(rif_id2)
+            self.client.sai_thrift_remove_virtual_router(vr_id)
+
+@group('acl')
+class SeqAclTableGroupTest(sai_base_test.ThriftInterfaceDataPlane):
+    def runTest(self):
+        print
+        print '----------------------------------------------------------------------------------------------'
+        print "Sending packet ptf_intf 2 -> ptf_intf 1 (192.168.0.1 ---> 10.10.10.1 [id = 105])"
+
+        switch_init(self.client)
+        port1 = port_list[1]
+        port2 = port_list[2]
+        v4_enabled = 1
+        v6_enabled = 1
+        mac = ''
+
+        vr_id = sai_thrift_create_virtual_router(self.client, v4_enabled, v6_enabled)
+        rif_id1 = sai_thrift_create_router_interface(self.client, vr_id, 1, port1, 0, v4_enabled, v6_enabled, mac)
+        rif_id2 = sai_thrift_create_router_interface(self.client, vr_id, 1, port2, 0, v4_enabled, v6_enabled, mac)
+
+        addr_family = SAI_IP_ADDR_FAMILY_IPV4
+        ip_addr1 = '10.10.10.1'
+        ip_mask1 = '255.255.255.255'
+        dmac1 = '00:11:22:33:44:55'
+        sai_thrift_create_neighbor(self.client, addr_family, rif_id1, ip_addr1, dmac1)
+        nhop1 = sai_thrift_create_nhop(self.client, addr_family, ip_addr1, rif_id1)
+        sai_thrift_create_route(self.client, vr_id, addr_family, ip_addr1, ip_mask1, rif_id1)
+
+        # send the test packet(s)
+        pkt = simple_tcp_packet(eth_dst=router_mac,
+            eth_src='00:22:22:22:22:22',
+            ip_dst='10.10.10.1',
+            ip_src='192.168.0.1',
+            ip_id=105,
+            ip_ttl=64)
+        exp_pkt = simple_tcp_packet(
+            eth_dst='00:11:22:33:44:55',
+            eth_src=router_mac,
+            ip_dst='10.10.10.1',
+            ip_src='192.168.0.1',
+            ip_id=105,
+            ip_ttl=63)
+        try:
+            print '#### NO ACL Applied ####'
+            print '#### Sending ', router_mac, '| 00:22:22:22:22:22 | 10.10.10.1 | 192.168.0.1 | @ ptf_intf 2'
+            send_packet(self, 2, str(pkt))
+            print '#### Expecting 00:11:22:33:44:55 |', router_mac, '| 10.10.10.1 | 192.168.0.1 | @ ptf_intf 1'
+            verify_packets(self, exp_pkt, [1])
+        finally:
+            print '----------------------------------------------------------------------------------------------'
+
+        print "Sending packet ptf_intf 2 -[acl]-> ptf_intf 1 (192.168.0.1 -[acl]-> 10.10.10.1 [id = 105])"
+        print 'ACL \'DROP, src mac 00:22:22:22:22:22, in_ports[ptf_intf_1,2]\' Applied '
+
+        # setup ACL table group
+        group_stage = SAI_ACL_STAGE_INGRESS
+        group_bind_point_list = [SAI_ACL_BIND_POINT_TYPE_PORT]
+        group_type = SAI_ACL_TABLE_GROUP_TYPE_SEQUENTIAL
+
+        # create ACL table group
+        acl_table_group_id = sai_thrift_create_acl_table_group(self.client,
+            group_stage,
+            group_bind_point_list,
+            group_type)
+
+        # setup ACL tables to block based on Source MAC
+        table_stage = SAI_ACL_STAGE_INGRESS
+        table_bind_point_list = [SAI_ACL_BIND_POINT_TYPE_PORT]
+        entry_priority = 1
+        action = SAI_PACKET_ACTION_DROP
+        in_ports = [port1, port2]
+        mac_src = '00:22:22:22:22:22'
+        mac_dst = None
+        mac_src_mask = None
+        mac_dst_mask = None
+        ip_proto = None
+        in_port = None
+        out_port = None
+        out_ports = None
+        ingress_mirror_id = None
+        egress_mirror_id = None
+
+        # create ACL table #1
+        acl_table_id1 = sai_thrift_create_acl_table(self.client,
+            table_stage,
+            table_bind_point_list,
+            addr_family,
+            mac_src,
+            mac_dst,
+            ip_src,
+            ip_dst,
+            ip_proto,
+            in_ports,
+            out_ports,
+            in_port,
+            out_port,
+            src_l4_port,
+            dst_l4_port)
+        acl_entry_id1 = sai_thrift_create_acl_entry(self.client,
+            acl_table_id,
+            entry_priority,
+            action, addr_family,
+            mac_src, mac_src_mask,
+            mac_dst, mac_dst_mask,
+            ip_src, ip_src_mask,
+            ip_dst, ip_dst_mask,
+            ip_proto,
+            in_ports, out_ports,
+            in_port, out_port,
+            src_l4_port, dst_l4_port,
+            ingress_mirror_id,
+            egress_mirror_id)
+
+        # create ACL table #2
+        acl_table_id2 = sai_thrift_create_acl_table(self.client,
+            table_stage,
+            table_bind_point_list,
+            addr_family,
+            mac_src,
+            mac_dst,
+            ip_src,
+            ip_dst,
+            ip_proto,
+            in_ports,
+            out_ports,
+            in_port,
+            out_port,
+            src_l4_port,
+            dst_l4_port)
+        acl_entry_id2 = sai_thrift_create_acl_entry(self.client,
+            acl_table_id,
+            entry_priority,
+            action, addr_family,
+            mac_src, mac_src_mask,
+            mac_dst, mac_dst_mask,
+            ip_src, ip_src_mask,
+            ip_dst, ip_dst_mask,
+            ip_proto,
+            in_ports, out_ports,
+            in_port, out_port,
+            src_l4_port, dst_l4_port,
+            ingress_mirror_id,
+            egress_mirror_id)
+
+        # setup ACL table group members
+        group_member_priority1 = 1
+        group_member_priority2 = 100
+
+        # create ACL table group members
+        acl_table_group_member_id1 = sai_thrift_create_acl_table_group(self.client,
+            acl_table_group_id,
+            acl_table_id1,
+            group_member_priority1)
+        acl_table_group_member_id2 = sai_thrift_create_acl_table_group(self.client,
+            acl_table_group_id,
+            acl_table_id2,
+            group_member_priority2)
+
+        # bind this ACL table group to port1s object id
+        attr_value = sai_thrift_attribute_value_t(oid=acl_table_group_id)
+        attr = sai_thrift_attribute_t(id=SAI_PORT_ATTR_INGRESS_ACL, value=attr_value)
+        self.client.sai_thrift_set_port_attribute(port1, attr)
+
+        try:
+            assert acl_table_group_id > 0, 'acl_table_group_id is <= 0'
+            assert acl_table_id1 > 0, 'acl_entry_id1 is <= 0'
+            assert acl_entry_id1 > 0, 'acl_entry_id1 is <= 0'
+            assert acl_table_id2 > 0, 'acl_entry_id2 is <= 0'
+            assert acl_entry_id2 > 0, 'acl_entry_id2 is <= 0'
+            assert acl_table_group_member_id1 > 0, 'acl_table_group_member_id1 is <= 0'
+            assert acl_table_group_member_id2 > 0, 'acl_table_group_member_id2 is <= 0'
+
+            print '#### ACL \'DROP, src mac 00:22:22:22:22:22, in_ports[ptf_intf_1,2]\' Applied ####'
+            print '#### Sending ', router_mac, '| 00:22:22:22:22:22 | 10.10.10.1 | 192.168.0.1 | @ ptf_intf 2'
+            # send the same packet
+            send_packet(self, 2, str(pkt))
+            # ensure packet is dropped
+            # check for absence of packet here!
+            print '#### NOT Expecting 00:11:22:33:44:55 |', router_mac, '| 10.10.10.1 | 192.168.0.1 | @ ptf_intf 1'
+            verify_no_packet(self, exp_pkt, 1)
+        finally:
+            # unbind this ACL table from port1s object id
+            attr_value = sai_thrift_attribute_value_t(oid=SAI_NULL_OBJECT_ID)
+            attr = sai_thrift_attribute_t(id=SAI_PORT_ATTR_INGRESS_ACL, value=attr_value)
+            self.client.sai_thrift_set_port_attribute(port1, attr)
+            # cleanup ACL
+            self.client.sai_thrift_remove_acl_table_group_member(acl_table_group_member_id1)
+            self.client.sai_thrift_remove_acl_table_group_member(acl_table_group_member_id2)
+            self.client.sai_thrift_remove_acl_entry(acl_entry_id1)
+            self.client.sai_thrift_remove_acl_table(acl_table_id1)
+            self.client.sai_thrift_remove_acl_entry(acl_entry_id2)
+            self.client.sai_thrift_remove_acl_table(acl_table_id2)
+            self.client.sai_thrift_remove_acl_table_group(acl_table_group_id)
             # cleanup
             sai_thrift_remove_route(self.client, vr_id, addr_family, ip_addr1, ip_mask1, rif_id1)
             self.client.sai_thrift_remove_next_hop(nhop1)
