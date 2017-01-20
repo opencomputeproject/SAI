8432e713362993226bbb3793463a94a66f75b5fc
Andriy Moroz <c_andriym@mellanox.com>
2017-01-20 15:38:42 +0200

    Add SAI_SWITCH_ATTR_MAX_ACL_RANGE_COUNT attribute to get number of ranges supported

--- inc/saiswitch.h
+++ inc/saiswitch.h
@@ -758,6 +758,14 @@ typedef enum _sai_switch_attr_t
     SAI_SWITCH_ATTR_MAX_ACL_ACTION_COUNT,
 
     /**
+     * @brief Count of the total number of ranges supported by NPU
+     *
+     * @type sai_uint32_t
+     * @flags READ_ONLY
+     */
+    SAI_SWITCH_ATTR_MAX_ACL_RANGE_COUNT,
+
+    /**
      * @brief Acl capabilities supported by the NPU
      *
      * @type sai_acl_capability_t
