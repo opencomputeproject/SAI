73db729f5272096f3fc3f8e52e377f1b2235c9a8
Itai Baz <itaib@mellanox.com>
2017-02-18 11:00:17 -0800

    Add missing switch ID to next hop group member (#421)

--- inc/sainexthopgroup.h
+++ inc/sainexthopgroup.h
@@ -199,6 +199,7 @@ typedef sai_status_t (*sai_get_next_hop_group_attribute_fn)(
  */
 typedef sai_status_t (*sai_create_next_hop_group_member_fn)(
     _Out_ sai_object_id_t* next_hop_group_member_id,
+    _In_ sai_object_id_t switch_id,
     _In_ uint32_t attr_count,
     _In_ const sai_attribute_t *attr_list
     );
