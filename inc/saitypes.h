3156e88d270962aa7266688f369ca62064c96584
Kamil Cudnik <kcudnik@gmail.com>
2017-03-06 02:55:53 -0800

    Make bulk operations parameters const (#437)

--- inc/saitypes.h
+++ inc/saitypes.h
@@ -611,8 +611,8 @@ typedef enum _sai_bulk_op_type_t {
 typedef sai_status_t (*sai_bulk_object_create_fn)(
         _In_ sai_object_id_t switch_id,
         _In_ uint32_t object_count,
-        _In_ uint32_t *attr_count,
-        _In_ sai_attribute_t **attrs,
+        _In_ const uint32_t *attr_count,
+        _In_ const sai_attribute_t **attrs,
         _In_ sai_bulk_op_type_t type,
         _Out_ sai_object_id_t *object_id,
         _Out_ sai_status_t *object_statuses);
@@ -632,7 +632,7 @@ typedef sai_status_t (*sai_bulk_object_create_fn)(
 
 typedef sai_status_t (*sai_bulk_object_remove_fn)(
         _In_ uint32_t object_count,
-        _In_ sai_object_id_t *object_id,
+        _In_ const sai_object_id_t *object_id,
         _In_ sai_bulk_op_type_t type,
         _Out_ sai_status_t *object_statuses);
 
