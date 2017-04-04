cc6430ef5fdae24e2ee8d76c4ced96d6cc1cabc5
Omer Shabtai <snifbh@gmail.com>
2017-04-04 17:27:08 +0300

    fixed logger for direct include of sai.h

--- Behavioral_Model/sai_adapter/src/sai_adapter.cpp
+++ Behavioral_Model/sai_adapter/src/sai_adapter.cpp
@@ -16,7 +16,7 @@ sai_adapter::sai_adapter()
   // logger
   logger_o = spdlog::get("logger");
   if (logger_o == 0) {
-    auto logger_o = spdlog::basic_logger_mt("logger", "logs/log.txt");
+    logger_o = spdlog::basic_logger_mt("logger", "logs/log.txt");
     logger_o->flush_on(spdlog::level::info);   // make err
     spdlog::set_pattern("[thread %t] %l %v "); // add %T for time
   }
