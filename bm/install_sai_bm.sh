6c75bc175f70360c6d6f9f3b1eef876782fe7ff9
Omer Shabtai <snifbh@gmail.com>
2017-05-03 17:14:57 +0300

    fixed target relocation

--- bm/install_sai_bm.sh
+++ bm/install_sai_bm.sh
@@ -4,6 +4,7 @@ mv -f behavioral-model/targets/Makefile.am Makefile_bak.am
 cp -f p4-sai/bm_config/configure.ac behavioral-model/configure.ac
 cp -f p4-sai/bm_config/Makefile.am behavioral-model/targets/Makefile.am
 
+rm -rf behavioral-model/targets/p4-sai
 mv p4-sai behavioral-model/targets/.
 # make behavioral model, p4-sai target.
 
@@ -14,5 +15,16 @@ make
 
 # return original config files 
 cd -
-mv -f p4-sai/bm_config/Makefile_bak.am behavioral-model/targets/Makefile.am 
-mv -f p4-sai/bm_config/configure_bak.ac behavioral-model/configure.ac 
+mv -f Makefile_bak.am behavioral-model/targets/Makefile.am 
+mv -f configure_bak.ac behavioral-model/configure.ac 
+
+# return compiled target
+mv behavioral-model/targets/p4-sai .
+
+# edit relative paths
+cp simple_switch simple_switch_bak
+sed -i 's/P4\/SAI\/bm\/behavioral-model\/targets\/p4-sai/p4-sai/g' simple_switch
+sed -i 's/\.\.\/\.\.\/src\/bm_runtime\/\.libs\/libbmruntime\.a/\.\.\/behavioral-model\/src\/bm_runtime\/\.libs\/libbmruntime\.a/g' simple_switch
+sed -i 's/\.\.\/\.\.\/thrift_src\/\.libs/\.\.\/behavioral-model\/thrift_src\/\.libs/g' simple_switch
+rm runtime_CLI
+ln -s ../behavioral-model/tools/runtime_CLI.py runtime_CLI
--- bm/p4-softswitch/pdfixed/thrift/res.thrift
+++
@@ -1,9 +0,0 @@
-namespace py res_pd_rpc
-namespace cpp res_pd_rpc
-namespace c_glib res_pd_rpc
-
-typedef i32 SessionHandle_t
-struct DevTarget_t {
-  1: required byte dev_id;
-  2: required i16 dev_pipe_id;
-}
