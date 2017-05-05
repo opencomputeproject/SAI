e5fd97bac4ca85f984cfb51fe9cc969a00cf2bc1
Kamil Cudnik <kcudnik@microsoft.com>
2017-05-05 10:25:16 -0700

    Fix doxygen comments

--- inc/saitypes.h
+++ inc/saitypes.h
@@ -594,14 +594,14 @@ typedef struct _sai_attribute_t
 
 typedef enum _sai_bulk_op_type_t
 {
-    /*
+    /**
      * @brief Bulk operation stops on the first failed creation
      *
      * Rest of objects will use SAI_STATUS_NON_EXECUTED return status value.
      */
     SAI_BULK_OP_TYPE_STOP_ON_ERROR,
 
-    /*
+    /**
      * @brief Bulk operation ignores the failures and continues to create other objects
      */
     SAI_BULK_OP_TYPE_INGORE_ERROR,
