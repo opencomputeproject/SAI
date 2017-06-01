7ec49b77aafea334eecb152b5eb169a6bd8db2ad
Kamil Cudnik <kcudnik@gmail.com>
2017-06-01 11:16:02 -0700

    Update saitypes.h

--- inc/saitypes.h
+++ inc/saitypes.h
@@ -614,7 +614,6 @@ typedef union _sai_attribute_value_t
     sai_s32_list_t s32list;
     sai_u32_range_t u32range;
     sai_s32_range_t s32range;
-    sai_map_list_t maplist;
     sai_vlan_list_t vlanlist;
     sai_qos_map_list_t qosmap;
     sai_acl_field_data_t aclfield;
