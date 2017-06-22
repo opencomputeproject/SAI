9ecce0a0281249bbc9b64117b677c19dd0aa8a92
shihjeff <jechen@cavium.com>
2017-06-22 13:08:21 -0700

    add FEC AN request attribute to saiport.h
    Signed-off-by: shihjeff <jechen@cavium.com>

--- inc/saiport.h
+++ inc/saiport.h
@@ -333,6 +333,14 @@ typedef enum _sai_port_attr_t
     SAI_PORT_ATTR_REMOTE_ADVERTISED_FEC_MODE,
 
     /**
+     * @brief Advertised remote port FEC AN request bit
+     *
+     * @type bool
+     * @flags READ_ONLY
+     */
+    SAI_PORT_ATTR_REMOTE_ADVERTISED_FEC_MODE_REQUEST,
+
+    /**
      * @brief Query list of Remote Port's Advertised HALF-Duplex speed in Mbps
      *
      * @type sai_u32_list_t
@@ -461,13 +469,22 @@ typedef enum _sai_port_attr_t
     SAI_PORT_ATTR_ADVERTISED_SPEED,
 
     /**
-     * @brief Query/Configure list of Advertised port FEC Mode
+     * @brief Query/Configure list of Advertised port FEC Mode. AN ability bit implicitly set to true.
      *
      * @type sai_s32_list_t sai_port_fec_mode_t
      * @flags CREATE_AND_SET
      * @default empty
      */
     SAI_PORT_ATTR_ADVERTISED_FEC_MODE,
+
+    /**
+     * @brief Advertised port FEC AN request bit.  True means FEC is requested
+     *
+     * @type bool
+     * @flags CREATE_AND_SET
+     * @default false
+     */
+    SAI_PORT_ATTR_ADVERTISED_FEC_MODE_REQUEST,
 
     /**
      * @brief Query/Configure list of Advertised HALF-Duplex speed in Mbps
