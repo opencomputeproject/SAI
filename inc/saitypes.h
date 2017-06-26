3e6410521c65991c10490f5fc3c89705ac47cb08
AshokDaparthi <ashok_daparthi@dell.com>
2017-06-26 01:34:48 -0700

    Added port pool type.

--- inc/saitypes.h
+++ inc/saitypes.h
@@ -231,7 +231,8 @@ typedef enum _sai_object_type_t
     SAI_OBJECT_TYPE_TAM_SNAPSHOT             = 62,
     SAI_OBJECT_TYPE_TAM_TRANSPORTER          = 63,
     SAI_OBJECT_TYPE_TAM_THRESHOLD            = 64,
-    SAI_OBJECT_TYPE_MAX                      = 65,
+    SAI_OBJECT_TYPE_PORT_POOL                = 65,
+    SAI_OBJECT_TYPE_MAX                      = 66,
 } sai_object_type_t;
 
 typedef struct _sai_u8_list_t
