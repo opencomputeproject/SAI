3cce9b67639f4c19411072cf5bfde198e602f687
karthik-krishnamurthy <Karthik_krishnamurt1@dell.com>
2017-07-14 11:52:43 +0530

    fix spell check(change subport to sub-port)

--- inc/saibridge.h
+++ inc/saibridge.h
@@ -388,9 +388,9 @@ typedef enum _sai_bridge_attr_t
      *
      * Provides control on the set of bridge ports on which unknown
      * unicast packets need to be flooded.If null object id is passed,
-     * then flooding will be disabled. Default is to flood to subports
+     * then flooding will be disabled. Default is to flood to sub-ports
      * of the bridge.During bridge create, SAI would internally create
-     * an L2MC group and whenever subports are added to the bridge, SAI
+     * an L2MC group and whenever sub-ports are added to the bridge, SAI
      * would automatically add them to that L2MC group.
      * Valid for SAI_BRIDGE_TYPE_1D.
      *
@@ -408,9 +408,9 @@ typedef enum _sai_bridge_attr_t
      *
      * Provides control on the set of bridge ports on which unknown
      * multicast packets need to be flooded.If null object id is passed,
-     * then flooding will be disabled. Default is to flood to subports
+     * then flooding will be disabled. Default is to flood on sub-ports
      * of the bridge.During bridge create, SAI would internally create
-     * an L2MC group and whenever subports are added to the bridge, SAI
+     * an L2MC group and whenever sub-ports are added to the bridge, SAI
      * would automatically add them to that L2MC group.
      * Valid for SAI_BRIDGE_TYPE_1D
      *
@@ -428,9 +428,9 @@ typedef enum _sai_bridge_attr_t
      *
      * Provides control on the set of bridge ports on which broadcast
      * packets need to be flooded.If null object id is passed,
-     * then flooding will be disabled. Default is to flood to subports
+     * then flooding will be disabled. Default is to flood to sub-ports
      * of the bridge.During bridge create, SAI would internally create
-     * an L2MC group and whenever subports are added to the bridge, SAI
+     * an L2MC group and whenever sub-ports are added to the bridge, SAI
      * would automatically add them to that L2MC group.
      * Valid for SAI_BRIDGE_TYPE_1D.
      *
