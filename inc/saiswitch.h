4a0213256ad2d9c41837b3592f0231fa0b29fcd0
Sudharsan Gopalarathinam <sudharsan_gopalarath@dell.com>
2017-07-31 21:55:54 -0700

    Fixing meta data failure

--- inc/saiswitch.h
+++ inc/saiswitch.h
@@ -947,13 +947,13 @@ typedef enum _sai_switch_attr_t
     SAI_SWITCH_ATTR_FDB_MULTICAST_MISS_PACKET_ACTION,
 
     /**
-     * @brief Action to be taken when a station movement is detected on static FDB entry
+     * @brief Action to be taken on packets when station move is observed with static FDB entry
      *
      * @type sai_packet_action_t
      * @flags CREATE_AND_SET
      * @default SAI_PACKET_ACTION_DROP
      */
-    SAI_SWITCH_ATTR_STATIC_FDB_MOVE_ACTION,
+    SAI_SWITCH_ATTR_STATIC_FDB_MOVE_PACKET_ACTION,
 
     /**
      * @brief SAI ECMP default hash algorithm
