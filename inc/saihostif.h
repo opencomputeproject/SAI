8e9d18cd3ff6e410d01a98f7f932fc78688d22b2
lguohan <lguohan@gmail.com>
2017-08-02 17:51:28 -0700

    add SAI_HOSTIF_ATTR_VLAN_TAG attribute to define whether to keep/strip (#699)
    vlan tag of the trapped packet

--- inc/saihostif.h
+++ inc/saihostif.h
@@ -591,6 +591,37 @@ typedef enum _sai_hostif_type_t
 } sai_hostif_type_t;
 
 /**
+ * @brief Attribute data for SAI_HOSTIF_ATTR_VLAN_TAG
+ */
+typedef enum _sai_hostif_vlan_tag_t
+{
+    /**
+     * @brief Strip vlan tag
+     * Strip vlan tag from the incoming packet
+     * when delivering the packet to host interface.
+     */
+    SAI_HOSTIF_VLAN_TAG_STRIP,
+
+    /**
+     * @brief Keep vlan tag.
+     * When incoming packet is untagged, add PVID tag to the packet when delivering
+     * the packet to host interface.
+     */
+    SAI_HOSTIF_VLAN_TAG_KEEP,
+
+    /**
+     * @brief Keep the packet same as the incoming packet
+     *
+     * The packet delivered to host interface is the same as the original packet.
+     * When the host interface is PORT and LAG, the packet delivered to host interface is the
+     * same as the original packet seen by the PORT and LAG.
+     * When the host interface is VLAN, the packet delivered to host interface will not have tag.
+     */
+    SAI_HOSTIF_VLAN_TAG_ORIGINAL,
+
+} sai_hostif_vlan_tag_t;
+
+/**
  * @brief Host interface attribute IDs
  */
 typedef enum _sai_hostif_attr_t
@@ -654,6 +685,16 @@ typedef enum _sai_hostif_attr_t
      * @default 0
      */
     SAI_HOSTIF_ATTR_QUEUE,
+
+    /**
+     * @brief Strip/keep vlan tag for received packet
+     *
+     * @type sai_hostif_vlan_tag_t
+     * @flags CREATE_AND_SET
+     * @default SAI_HOSTIF_VLAN_TAG_STRIP
+     * @validonly SAI_HOSTIF_ATTR_TYPE == SAI_HOSTIF_TYPE_NETDEV
+     */
+    SAI_HOSTIF_ATTR_VLAN_TAG,
 
     /**
      * @brief End of attributes
