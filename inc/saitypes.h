22b327645070f78fcf76b0a4d007aa17fd6b86d6
Ragu Raman S <Ragu.RamanS@dell.com>
2017-12-14 03:02:13 -0800

    Added BFD object type

--- inc/saitypes.h
+++ inc/saitypes.h
@@ -238,7 +238,8 @@ typedef enum _sai_object_type_t
     SAI_OBJECT_TYPE_INSEG_ENTRY              = 67,
     SAI_OBJECT_TYPE_TAM_HISTOGRAM            = 68,
     SAI_OBJECT_TYPE_TAM_MICROBURST           = 69,
-    SAI_OBJECT_TYPE_MAX                      = 70,
+    SAI_OBJECT_TYPE_BFD                      = 70,
+    SAI_OBJECT_TYPE_MAX                      = 71,
 } sai_object_type_t;
 
 typedef struct _sai_u8_list_t
