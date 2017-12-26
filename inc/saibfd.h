68a766dba13b99ceac8cbdbada2241cc4a5b1bc7
Ragu Raman S <Ragu.RamanS@dell.com>
2017-12-26 03:01:14 -0800

    meta fix

--- inc/saibfd.h
+++ inc/saibfd.h
@@ -228,7 +228,6 @@ typedef enum _sai_bfd_session_attr_t
      * @type bool
      * @flags CREATE_ONLY
      * @default false
-     * @condition SAI_BFD_SESSION_ATTR_HW_LOOKUP_VALID == false
      */
     SAI_BFD_SESSION_ATTR_VLAN_HEADER_VALID,
 
