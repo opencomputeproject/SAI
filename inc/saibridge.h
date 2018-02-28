f3aebca30609c3b31544d24d6d9a5d05b6c8ff66
Karthik Krishnamurthy <karthik-krishnamurthy@users.noreply.github.com>
2018-02-28 19:05:22 +0530

    Adding egress filtering enable/disable support

--- inc/saibridge.h
+++ inc/saibridge.h
@@ -234,6 +234,15 @@ typedef enum _sai_bridge_port_attr_t
      * @default false
      */
     SAI_BRIDGE_PORT_ATTR_INGRESS_FILTERING,
+    
+    /**
+     * @brief Egress filtering (drop frames with unknown VLANs at egress)
+     *
+     * @type bool
+     * @flags CREATE_AND_SET
+     * @default false
+     */
+    SAI_BRIDGE_PORT_ATTR_EGRESS_FILTERING,
 
     /**
      * @brief End of attributes
