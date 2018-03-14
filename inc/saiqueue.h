0f217b33e03b206ca2fe0ceed7f0864142b581cd
Itai Baz <itaib@mellanox.com>
2018-03-14 14:24:35 -0700

    Add extended statistics get functions
    Extended funtions have extra mode paramter. For now the options are
    regular read and read with clear. Read and clear allows atomic operation
    without loosing counters in the gap between the read and clear functions
    in existing API.
    Mode can be furthermore extended upon future need.
    Old get statistics functions are deprecated but left for backward
    compatability

--- inc/saiqueue.h
+++ inc/saiqueue.h
@@ -396,7 +396,7 @@ typedef sai_status_t (*sai_get_queue_attribute_fn)(
         _Inout_ sai_attribute_t *attr_list);
 
 /**
- * @brief Get queue statistics counters.
+ * @brief Get queue statistics counters. Deprecated for backward compatibility.
  *
  * @param[in] queue_id Queue id
  * @param[in] number_of_counters Number of counters in the array
@@ -409,6 +409,24 @@ typedef sai_status_t (*sai_get_queue_stats_fn)(
         _In_ sai_object_id_t queue_id,
         _In_ uint32_t number_of_counters,
         _In_ const sai_queue_stat_t *counter_ids,
+        _Out_ uint64_t *counters);
+
+/**
+ * @brief Get queue statistics counters extended.
+ *
+ * @param[in] queue_id Queue id
+ * @param[in] number_of_counters Number of counters in the array
+ * @param[in] counter_ids Specifies the array of counter ids
+ * @param[in] mode Statistics mode
+ * @param[out] counters Array of resulting counter values.
+ *
+ * @return #SAI_STATUS_SUCCESS on success, failure status code on error
+ */
+typedef sai_status_t (*sai_get_queue_stats_ext_fn)(
+        _In_ sai_object_id_t queue_id,
+        _In_ uint32_t number_of_counters,
+        _In_ const sai_queue_stat_t *counter_ids,
+        _In_ sai_stats_mode_t mode,
         _Out_ uint64_t *counters);
 
 /**
@@ -449,6 +467,7 @@ typedef struct _sai_queue_api_t
     sai_set_queue_attribute_fn   set_queue_attribute;
     sai_get_queue_attribute_fn   get_queue_attribute;
     sai_get_queue_stats_fn       get_queue_stats;
+    sai_get_queue_stats_ext_fn   get_queue_stats_ext;
     sai_clear_queue_stats_fn     clear_queue_stats;
 
 } sai_queue_api_t;
