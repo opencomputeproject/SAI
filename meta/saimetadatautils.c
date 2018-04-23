e6d1905da9dbfc2a1c8a2eca1bdbaa7579aca3b2
Kamil Cudnik <kcudnik@gmail.com>
2018-04-23 18:17:42 +0200

    Add support  for experimental attributes (#807)
    * Add supporf for experimental attributes
    
    * Fix comments
    
    * Remove example fpga extensions

--- meta/saimetadatautils.c
+++ meta/saimetadatautils.c
@@ -209,7 +209,7 @@ const sai_object_type_info_t* sai_metadata_get_object_type_info(
 bool sai_metadata_is_object_type_valid(
         _In_ sai_object_type_t object_type)
 {
-    return object_type > SAI_OBJECT_TYPE_NULL && object_type < SAI_OBJECT_TYPE_MAX;
+    return object_type > SAI_OBJECT_TYPE_NULL && object_type < SAI_OBJECT_TYPE_EXTENSIONS_MAX;
 }
 
 bool sai_metadata_is_condition_met(
