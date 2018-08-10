9b84d6a8d56d54e2239cb28e45941e7ee06960e1
JaiOCP <42279853+JaiOCP@users.noreply.github.com>
2018-08-10 11:10:54 -0700

    Update saitam.h

--- inc/saitam.h
+++ inc/saitam.h
@@ -1,6 +1,7 @@
 /**
  * Copyright (c) 2014 Microsoft Open Technologies, Inc.
  *
+ *    
  *    Licensed under the Apache License, Version 2.0 (the "License"); you may
  *    not use this file except in compliance with the License. You may obtain
  *    a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
