47887dcc8615f9d9d14d46ee95c25ea47b22ac8d
Jemston Fernando <jemston_fernando@dell.com>
2018-09-26 22:32:02 -0700

    Addressed review comments to add a new attribute that support object list (for backward compatibility)

--- inc/saimirror.h
+++ inc/saimirror.h
@@ -94,12 +94,11 @@ typedef enum _sai_mirror_session_attr_t
 
     /**
      * @brief Destination/Analyzer/Monitor Port.
-     * null valid only for remote session and adds all VLAN members as monitor port
      *
      * @type sai_object_id_t
      * @flags MANDATORY_ON_CREATE | CREATE_AND_SET
      * @objects SAI_OBJECT_TYPE_PORT, SAI_OBJECT_TYPE_LAG
-     * @allownull true
+     * @condition SAI_MIRROR_SESSION_ATTR_MONITOR_PORTLIST_VALID == false
      */
     SAI_MIRROR_SESSION_ATTR_MONITOR_PORT,
 
@@ -290,6 +289,25 @@ typedef enum _sai_mirror_session_attr_t
      * @condition SAI_MIRROR_SESSION_ATTR_TYPE == SAI_MIRROR_SESSION_TYPE_ENHANCED_REMOTE
      */
     SAI_MIRROR_SESSION_ATTR_GRE_PROTOCOL_TYPE,
+
+    /**
+     * @brief Monitor port list is valid
+     *
+     * @type bool
+     * @flags CREATE_ONLY
+     * @default false
+     */
+    SAI_MIRROR_SESSION_ATTR_MONITOR_PORTLIST_VALID,
+
+    /**
+     * @brief Destination/Analyzer/Monitor Port List.
+     *
+     * @type sai_object_list_t
+     * @flags MANDATORY_ON_CREATE | CREATE_AND_SET
+     * @objects SAI_OBJECT_TYPE_PORT, SAI_OBJECT_TYPE_LAG
+     * @condition SAI_MIRROR_SESSION_ATTR_MONITOR_PORTLIST_VALID == true
+     */
+    SAI_MIRROR_SESSION_ATTR_MONITOR_PORTLIST,
 
     /**
      * @brief End of attributes
