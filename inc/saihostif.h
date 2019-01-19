67bde0d99db9f4c1091a895a93cf572b61043064
Itai Baz <itaib@mellanox.com>
2019-01-18 17:16:21 -0800

    spelling

--- inc/saihostif.h
+++ inc/saihostif.h
@@ -237,8 +237,8 @@ typedef enum _sai_hostif_trap_type_t
     SAI_HOSTIF_TRAP_TYPE_ARP_RESPONSE = 0x00002001,
 
     /**
-     * @brief DHCP traffic (UDP ports 67, 68), either L3 broadcast or UC to
-     * local router IP address (default packet action is forward)
+     * @brief DHCP traffic (UDP ports 67, 68), either L3 broadcast or unicast
+     * to local router IP address (default packet action is forward)
      */
     SAI_HOSTIF_TRAP_TYPE_DHCP = 0x00002002,
 
