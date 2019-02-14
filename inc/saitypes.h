5d87f5f7e7e30b23c3c54a132ff511626b42e44b
Jai Kumar <jai.kumar@broadom.com>
2019-02-14 13:02:34 -0800

    Removed TAM INT API from this PR.

--- inc/saitypes.h
+++ inc/saitypes.h
@@ -257,8 +257,7 @@ typedef enum _sai_object_type_t
     SAI_OBJECT_TYPE_TAM_COLLECTOR            = 78,
     SAI_OBJECT_TYPE_TAM_EVENT_ACTION         = 79,
     SAI_OBJECT_TYPE_TAM_EVENT                = 80,
-    SAI_OBJECT_TYPE_TAM_INT                  = 81,
-    SAI_OBJECT_TYPE_MAX                      = 82,
+    SAI_OBJECT_TYPE_MAX                      = 81,
 } sai_object_type_t;
 
 typedef struct _sai_u8_list_t
