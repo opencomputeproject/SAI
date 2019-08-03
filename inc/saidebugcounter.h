70a3a3c5f0c342defbd9666473693bcbf6f82d86
Itai Baz <itaib@mellanox.com>
2019-08-03 02:44:56 -0700

    fix meta

--- inc/saidebugcounter.h
+++ inc/saidebugcounter.h
@@ -107,12 +107,12 @@ typedef enum _sai_port_in_drop_reason_t
     /** IPv4 Source IP is link local (Source IP=169.254.0.0/16) */
     SAI_PORT_IN_DROP_REASON_SIP_LINK_LOCAL,
 
-    /** packet size is larger than the MTU */
+    /** Packet size is larger than the MTU */
     SAI_PORT_IN_DROP_REASON_EXCEEDS_MTU,
 
     /* ACL reasons */
 
-    /** packet is dropped due to configured ACL rules */
+    /** Packet is dropped due to configured ACL rules */
     SAI_PORT_IN_DROP_REASON_ACL_DISCARD
 
 } sai_port_in_drop_reason_t;
@@ -125,7 +125,7 @@ typedef enum _sai_port_out_drop_reason_t
     /** Egress VLAN filter */
     SAI_PORT_OUT_DROP_REASON_EGRESS_VLAN_FILTER,
 
-    /** packet is destined for neighboring device but neighbor device link is down */
+    /** Packet is destined for neighboring device but neighbor device link is down */
     SAI_PORT_OUT_DROP_REASON_L3_EGRESS_LINK_DOWN,
 
 } sai_port_out_drop_reason_t;
