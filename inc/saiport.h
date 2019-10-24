f15b6bfea305afcdd5f25eaa2b387dfa700d4e75
Danny Allen <52468448+daall@users.noreply.github.com>
2019-10-24 14:46:37 -0700

    Improve debug counter enums (#1006)
    * Improve debug counter enums
    * Add START and END attributes for drop reason enums
    * Add additional port stats for debug counters
    * Add additional switch stats for debug counters
    * Assign enum start value to first drop reason

--- inc/saiport.h
+++ inc/saiport.h
@@ -1741,11 +1741,53 @@ typedef enum _sai_port_stat_t
     /** Port stat in drop reasons range start */
     SAI_PORT_STAT_IN_DROP_REASON_RANGE_BASE = 0x00001000,
 
+    /** Get in port packet drops configured by debug counter API at index 1 */
+    SAI_PORT_STAT_IN_CONFIGURED_DROP_REASONS_1_DROPPED_PKTS,
+
+    /** Get in port packet drops configured by debug counter API at index 2 */
+    SAI_PORT_STAT_IN_CONFIGURED_DROP_REASONS_2_DROPPED_PKTS,
+
+    /** Get in port packet drops configured by debug counter API at index 3 */
+    SAI_PORT_STAT_IN_CONFIGURED_DROP_REASONS_3_DROPPED_PKTS,
+
+    /** Get in port packet drops configured by debug counter API at index 4 */
+    SAI_PORT_STAT_IN_CONFIGURED_DROP_REASONS_4_DROPPED_PKTS,
+
+    /** Get in port packet drops configured by debug counter API at index 5 */
+    SAI_PORT_STAT_IN_CONFIGURED_DROP_REASONS_5_DROPPED_PKTS,
+
+    /** Get in port packet drops configured by debug counter API at index 6 */
+    SAI_PORT_STAT_IN_CONFIGURED_DROP_REASONS_6_DROPPED_PKTS,
+
+    /** Get in port packet drops configured by debug counter API at index 7 */
+    SAI_PORT_STAT_IN_CONFIGURED_DROP_REASONS_7_DROPPED_PKTS,
+
     /** Port stat in drop reasons range end */
     SAI_PORT_STAT_IN_DROP_REASON_RANGE_END = 0x00001fff,
 
     /** Port stat out drop reasons range start */
     SAI_PORT_STAT_OUT_DROP_REASON_RANGE_BASE = 0x00002000,
+
+    /** Get out port packet drops configured by debug counter API at index 1 */
+    SAI_PORT_STAT_OUT_CONFIGURED_DROP_REASONS_1_DROPPED_PKTS,
+
+    /** Get out port packet drops configured by debug counter API at index 2 */
+    SAI_PORT_STAT_OUT_CONFIGURED_DROP_REASONS_2_DROPPED_PKTS,
+
+    /** Get out port packet drops configured by debug counter API at index 3 */
+    SAI_PORT_STAT_OUT_CONFIGURED_DROP_REASONS_3_DROPPED_PKTS,
+
+    /** Get out port packet drops configured by debug counter API at index 4 */
+    SAI_PORT_STAT_OUT_CONFIGURED_DROP_REASONS_4_DROPPED_PKTS,
+
+    /** Get out port packet drops configured by debug counter API at index 5 */
+    SAI_PORT_STAT_OUT_CONFIGURED_DROP_REASONS_5_DROPPED_PKTS,
+
+    /** Get out port packet drops configured by debug counter API at index 6 */
+    SAI_PORT_STAT_OUT_CONFIGURED_DROP_REASONS_6_DROPPED_PKTS,
+
+    /** Get out port packet drops configured by debug counter API at index 7 */
+    SAI_PORT_STAT_OUT_CONFIGURED_DROP_REASONS_7_DROPPED_PKTS,
 
     /** Port stat out drop reasons range end */
     SAI_PORT_STAT_OUT_DROP_REASON_RANGE_END = 0x00002fff,
