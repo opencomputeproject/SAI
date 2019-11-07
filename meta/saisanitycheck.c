6e4fbf49fa2a6c73ff220ac3ef41c9cedf63f91f
Ashok Daparthi-Dell <Ashok_Daparthi@Dell.com>
2019-11-06 17:09:44 -0800

    Gearbox spec (#1014)

--- meta/saisanitycheck.c
+++ meta/saisanitycheck.c
@@ -918,7 +918,13 @@ void check_attr_default_required(
             META_MD_ASSERT_FAIL(md, "default value list is needed on this attr value type but list is NULL");
 
         case SAI_ATTR_VALUE_TYPE_POINTER:
-            break;
+
+            /*
+             * Gearbox exception for mandatory pointer attribute
+             * to support CONST on list.
+             */
+
+           break;
 
         default:
 
@@ -1665,7 +1671,9 @@ void check_attr_key(
             case SAI_ATTR_VALUE_TYPE_OBJECT_ID:
 
                 if ((md->objecttype == SAI_OBJECT_TYPE_QUEUE && md->attrid == SAI_QUEUE_ATTR_PORT) ||
-                        (md->objecttype == SAI_OBJECT_TYPE_INGRESS_PRIORITY_GROUP && md->attrid == SAI_INGRESS_PRIORITY_GROUP_ATTR_PORT))
+                    (md->objecttype == SAI_OBJECT_TYPE_INGRESS_PRIORITY_GROUP && md->attrid == SAI_INGRESS_PRIORITY_GROUP_ATTR_PORT) ||
+                    (md->objecttype == SAI_OBJECT_TYPE_PORT_CONNECTOR && md->attrid == SAI_PORT_CONNECTOR_ATTR_SYSTEM_SIDE_PORT_ID) ||
+                    (md->objecttype == SAI_OBJECT_TYPE_PORT_CONNECTOR && md->attrid == SAI_PORT_CONNECTOR_ATTR_LINE_SIDE_PORT_ID))
                 {
                     /*
                      * This is also special case, OBJECT_ID at should not be a
@@ -2290,6 +2298,11 @@ void check_attr_existing_objects(
 
             META_MD_ASSERT_FAIL(md, "object list is not supported on this object type");
 
+        case SAI_ATTR_VALUE_TYPE_POINTER:
+            /*
+             * Allow poniter for switch register read and write API's.
+             */
+            break;
         default:
 
             META_MD_ASSERT_FAIL(md, "not supported attr value type on existing object");
@@ -4019,8 +4032,8 @@ void check_switch_attributes()
     for (; meta[index] != NULL; index++)
     {
         const sai_attr_metadata_t *md = meta[index];
-
-        if (md->isconditional || md->isvalidonly)
+        /* Gearbox added validonly attributes at switch */
+        if (md->isconditional)
         {
             META_MD_ASSERT_FAIL(md, "attribute can't be conditional/validonly (this check can be relaxed)");
         }
