26b1470e18623d74b196b804e98e20785613dde7
prafull-brcm <56103377+prafull-brcm@users.noreply.github.com>
2020-01-21 14:14:07 +0530

    Update saimacsec.h

--- inc/saimacsec.h
+++ inc/saimacsec.h
@@ -470,22 +470,22 @@ typedef enum _sai_macsec_flow_attr_t
 typedef enum _sai_macsec_flow_stat_t
 {
     /**
-     * @brief packets dropped IfIn/IfOut before MACsec processing for uncontrolled port
+     * @brief packets dropped in IfIn or IfOut operation before MACsec processing for uncontrolled port
      */
     SAI_MACSEC_FLOW_STAT_RX_DROP_PKTS_UNCONTROLLED,
 
     /**
-     * @brief packets dropped IfIn/IfOut before MACsec processing for controlled port
+     * @brief packets dropped in IfIn or IfOut before MACsec processing for controlled port
      */
     SAI_MACSEC_FLOW_STAT_RX_DROP_PKTS_CONTROLLED,
 
     /**
-     * @brief Packet counters - In/Out Rx Error for uncontrolled port
+     * @brief Packet counters In or Out Rx Error for uncontrolled port
      */
     SAI_MACSEC_FLOW_STAT_RX_ERR_PKTS_UNCONTROLLED,
 
     /**
-     * @brief Packet counters - In/Out Rx Error for controlled port
+     * @brief Packet counters In or Out Rx Error for controlled port
      */
     SAI_MACSEC_FLOW_STAT_RX_ERR_PKTS_CONTROLLED,
 
