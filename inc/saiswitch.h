4c4b07331c6a24c0820b4e6e7d9014b9aa6e88c7
kcudnik <kcudnik@gmail.com>
2020-01-28 13:11:33 +0100

    Add missing in/out counters index zero

--- inc/saiswitch.h
+++ inc/saiswitch.h
@@ -2122,6 +2122,9 @@ typedef enum _sai_switch_stat_t
     /** Switch stat in drop reasons range start */
     SAI_SWITCH_STAT_IN_DROP_REASON_RANGE_BASE = 0x00001000,
 
+    /** Get in switch packet drops configured by debug counter API at index 0 */
+    SAI_SWITCH_STAT_IN_CONFIGURED_DROP_REASONS_0_DROPPED_PKTS = SAI_SWITCH_STAT_IN_DROP_REASON_RANGE_BASE,
+
     /** Get in switch packet drops configured by debug counter API at index 1 */
     SAI_SWITCH_STAT_IN_CONFIGURED_DROP_REASONS_1_DROPPED_PKTS,
 
@@ -2148,6 +2151,9 @@ typedef enum _sai_switch_stat_t
 
     /** Switch stat out drop reasons range start */
     SAI_SWITCH_STAT_OUT_DROP_REASON_RANGE_BASE = 0x00002000,
+
+    /** Get out switch packet drops configured by debug counter API at index 0 */
+    SAI_SWITCH_STAT_OUT_CONFIGURED_DROP_REASONS_0_DROPPED_PKTS = SAI_SWITCH_STAT_OUT_DROP_REASON_RANGE_BASE,
 
     /** Get out switch packet drops configured by debug counter API at index 1 */
     SAI_SWITCH_STAT_OUT_CONFIGURED_DROP_REASONS_1_DROPPED_PKTS,
