92084e63792d4a8180352a7c77acc7a7d6c39e46
pshaikh <pshaikh@fb.com>
2020-04-02 11:05:16 -0700

    applying review comments

--- inc/saihostif.h
+++ inc/saihostif.h
@@ -397,25 +397,25 @@ typedef enum _sai_hostif_trap_type_t
      */
     SAI_HOSTIF_TRAP_TYPE_PIPELINE_DISCARD_ROUTER = 0x00007002,
 
+    /** Exception traps custom range start */
+    SAI_HOSTIF_TRAP_TYPE_CUSTOM_EXCEPTION_RANGE_BASE = 0x00008000,
+
     /**
      * @brief MPLS packets with expiring TTL value of 1
      * (default packet action is drop)
      */
-    SAI_HOSTIF_TRAP_TYPE_MPLS_TTL_ERROR = 0x00007500,
+    SAI_HOSTIF_TRAP_TYPE_MPLS_TTL_ERROR = 0x00009000,
 
     /**
      * @brief MPLS packet with router alert label
      * (default packet action is forward)
      */
-    SAI_HOSTIF_TRAP_TYPE_MPLS_ROUTER_ALERT_LABEL = 0x00007501,
-
-    /** Exception traps custom range start */
-    SAI_HOSTIF_TRAP_TYPE_CUSTOM_EXCEPTION_RANGE_BASE = 0x00008000,
+    SAI_HOSTIF_TRAP_TYPE_MPLS_ROUTER_ALERT_LABEL = 0x00009001,
 
     /**
      * @brief End of trap types
      */
-    SAI_HOSTIF_TRAP_TYPE_END = 0x00009000
+    SAI_HOSTIF_TRAP_TYPE_END = 0x0000a000
 
 } sai_hostif_trap_type_t;
 
