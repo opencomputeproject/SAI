d054d09a3f4654c59f008b192358bdbdf1755fe5
Itai Baz <itaib@mellanox.com>
2020-07-28 19:59:19 -0700

    Add DHCP L2 trap

--- inc/saihostif.h
+++ inc/saihostif.h
@@ -230,6 +230,12 @@ typedef enum _sai_hostif_trap_type_t
      */
     SAI_HOSTIF_TRAP_TYPE_PTP_TX_EVENT = 0x00000011,
 
+    /**
+     * @brief DHCP traffic (UDP ports 67, 68) 
+     * (default packet action is drop)
+     */
+    SAI_HOSTIF_TRAP_TYPE_DHCP_L2 = 0x00000012,
+
     /** Switch traps custom range start */
     SAI_HOSTIF_TRAP_TYPE_SWITCH_CUSTOM_RANGE_BASE = 0x00001000,
 
