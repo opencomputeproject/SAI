58f8f1e44324cd673967def5975a9c2e95abe9f3
Kamil Cudnik <kcudnik@gmail.com>
2020-11-25 21:25:22 +0100

    Add label attribute for LAG and virtual router (#1158)
    Since LAG and virtual router can be created without any unique mandatory attributes, anchor attribute can be used to uniquely identify specified objects. This will be very useful in SONiC warm boot scenario, for example to identify 2 empty LAGs which are present on the device after reboot.
    
    This attribute doesn't correspond to any internal SAI vendor device resources, and at this point it don't need to be implemented by any vendor, it implementation at create function internals CAN be skipped without posting error. This attribute is considered as user data attached to specific object.
    If vendor wants to support this attribute it should also be persistent over warm boot scenario
    
    Cons: extra attribute, that has no actual resource meaning and it would need to be added to objects that need to be uniquely identified on user side
    
    Pros: convenient for SONiC, since metadata for all attributes is auto generated, and it will make very easy to to use existing attribute, instead of making convoluted way to pass extra information from sairedis/syncd path (this is because of current SONiC architecture design in sairedis/syncd model)
    
    Related to:
    #1080
    #1132
    
    Signed-off-by: Jasmeet Bagga <jasmeetbagga@fb.com>

--- inc/saivirtualrouter.h
+++ inc/saivirtualrouter.h
@@ -107,6 +107,15 @@ typedef enum _sai_virtual_router_attr_t
     SAI_VIRTUAL_ROUTER_ATTR_UNKNOWN_L3_MULTICAST_PACKET_ACTION,
 
     /**
+     * @brief Label attribute used to unique identify empty VR.
+     *
+     * @type char
+     * @flags CREATE_AND_SET
+     * @default ""
+     */
+    SAI_VIRTUAL_ROUTER_ATTR_LABEL,
+
+    /**
      * @brief End of attributes
      */
     SAI_VIRTUAL_ROUTER_ATTR_END,
