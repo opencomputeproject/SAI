ffcedff74ecb5d78038ecfeb3516cba541b4eaaa
kcudnik <kcudnik@gmail.com>
2021-09-24 12:38:08 +0200

    Remove cat

--- meta/checkancestry.sh
+++ meta/checkancestry.sh
@@ -91,7 +91,7 @@ function create_commit_list()
 
     echo "ancestry graph"
 
-    git --no-pager log --graph --oneline --ancestry-path  origin/master^..HEAD | cat
+    git --no-pager log --graph --oneline --ancestry-path  origin/master^..HEAD
 
     echo "git rev list from $begin to $end"
 
