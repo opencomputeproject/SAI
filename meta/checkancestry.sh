f3f1f2ca0058bafed27a41402759bee1251b5797
Kamil Cudnik <kcudnik@gmail.com>
2021-09-24 12:22:44 +0200

    [meta] Display ancestry graph for build (#1315)

--- meta/checkancestry.sh
+++ meta/checkancestry.sh
@@ -89,6 +89,10 @@ function create_commit_list()
     local begin=$1
     local end=$2
 
+    echo "ancestry graph"
+
+    git log --graph --oneline --ancestry-path  origin/master^..HEAD | cat
+
     echo "git rev list from $begin to $end"
 
     LIST=$(git rev-list --ancestry-path ${begin}^..${end} | xargs -n 1 git rev-parse --short | tac)
