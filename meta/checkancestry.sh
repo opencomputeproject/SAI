376270c41dad52a06dec894703200897d6db53fb
kcudnik <kcudnik@gmail.com>
2021-09-24 12:33:03 +0200

    [meta] Add no pager for git log

--- meta/checkancestry.sh
+++ meta/checkancestry.sh
@@ -91,7 +91,7 @@ function create_commit_list()
 
     echo "ancestry graph"
 
-    git log --graph --oneline --ancestry-path  origin/master^..HEAD | cat
+    git --no-pager log --graph --oneline --ancestry-path  origin/master^..HEAD | cat
 
     echo "git rev list from $begin to $end"
 
