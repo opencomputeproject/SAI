877db9b5cd45cb015d431b601be806708bef8292
Richard.Yu <richard.yu@microsoft.com>
2022-02-18 15:18:48 +0800

    Add platform support and a sanity test sample (#1390)
    * Add a middle layer (strategy and factory) under the test cases which can
    
    no more if-else for platform selecting
    no more code injection for different platforms, just need to abstract to a method on the differences
    auto select the platform by the parameters in the test starter (shell, happed in run_p4_tests, not published here)
    easy-distinct structure for the difference from platforms
    prepared for future statistic
    Add a sanity sample, it can
    
    configure across all the ports (base on port's configuration file)
    simple FDB and VLAN naming according to port's number
    make a basic check on the status of the devices
    use as a sample for how to use the new middle layer
    
    fix and suppress LGTM alters
    
    Signed-off-by: Richard Yu <richard.yu@microsoft.com>
    
    * code refactor part I
    
    - add comments for class and method
    - reformat code
    - move platform related method to corresponding class
    
    code refactor Part II
    
    optimize the class hirerarchy
    simplify the import items
    add more docs
    
    Signed-off-by: richardyu-ms <richard.yu@microsoft.com>
    
    * suppress lgtm warnings
    
    fix lgtm warning
    
    Signed-off-by: richardyu-ms <richard.yu@microsoft.com>

---
+++ ptf/platform_helper/__init__.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2021 Microsoft Open Technologies, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+#    THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR
+#    CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT
+#    LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS
+#    FOR A PARTICULAR PURPOSE, MERCHANTABILITY OR NON-INFRINGEMENT.
+#
+#    See the Apache Version 2.0 License for specific language governing
+#    permissions and limitations under the License.
+#
+#    Microsoft would like to thank the following companies for their review and
+#    assistance with these files: Intel Corporation, Mellanox Technologies Ltd,
+#    Dell Products, L.P., Facebook, Inc., Marvell International Ltd.
+#
+# @file    __init__.py
+#
+# @brief   init
+#
+
+"""
+Init the platform helper module.
+
+platform_helper module contains classes to distiguish the different behivor on different platforms.
+"""
