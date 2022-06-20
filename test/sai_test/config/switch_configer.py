251ba811326b695dcaca71a0d42e50a82a2f0aa7
Richard.Yu <richard.yu@microsoft.com>
2022-06-20 16:30:55 +0800

    [SAI-PTF] SAI PTF test structure for T0 test and VLAN,FDB sample cases (#1499)
    * [SAI-PTF] Add SAI ptf test structure for T0 test
    
    Create SAI PTF test structure for T0
    1. Create configuration structure for different components configu
    2. Add FDB, PORT, switch, vlan configurations
    3. Create Test structure base on PTF
    4. Add T0 test case samples for FDB
    
    Test Done:
    Checked the ports functionality with port flooding
    Checked port forwarding base on FDB configurations
    
    Signed-off-by: richardyu-ms <richard.yu@microsoft.com>
    
    * add vlan config and vlan test sample case
    
    Signed-off-by: richardyu-ms <richard.yu@microsoft.com>
    
    * add dx010 config
    
    Signed-off-by: richardyu-ms <richard.yu@microsoft.com>

---
+++ test/sai_test/config/switch_configer.py
@@ -0,0 +1,72 @@
+# Copyright (c) 2021 Microsoft Open Technologies, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+#    THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR
+#    CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT
+#    LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS
+#    FOR A PARTICULAR PURPOSE, MERCHANTABILITY OR NON-INFRINGEMENT.
+#
+#    See the Apache Version 2.0 License for specific language governing
+#    permissions and limitations under the License.
+#
+#    Microsoft would like to thank the following companies for their review and
+#    assistance with these files: Intel Corporation, Mellanox Technologies Ltd,
+#    Dell Products, L.P., Facebook, Inc., Marvell International Ltd.
+#
+
+
+from sai_thrift.sai_adapter import *
+from sai_utils import *  # pylint: disable=wildcard-import; lgtm[py/polluting-import]
+
+
+def t0_switch_config_helper(test_obj):
+    """
+    Make t0 switch configurations base on the configuration in the test plan.
+    Set the configuration in test directly.
+
+    Set the following test_obj attributes:
+        int: switch_id
+
+    """
+    configer = SwitchConfiger(test_obj)
+    test_obj.switch_id = configer.start_switch()
+
+
+class SwitchConfiger(object):
+    """
+    Class use to make all the Switch configurations.
+    """
+
+    def __init__(self, test_obj) -> None:
+        """
+        Init the Switch configer.
+
+        Args:
+            test_obj: the test object
+        """
+
+        self.test_obj = test_obj
+        self.client = test_obj.client
+
+    def start_switch(self, switch_init_wait=1, route_mac=ROUTER_MAC):
+        """
+        Start switch and wait seconds for a warm up.
+
+        Args:
+            switch_init_wait: switch init wait time (sec)
+            route_mac: route mac (switch mac)
+
+        Returns:
+            Vlan: vlan object
+        """
+        switch_id = sai_thrift_create_switch(
+            self.test_obj.client, init_switch=True, src_mac_address=route_mac)
+        self.test_obj.assertEqual(self.test_obj.status(), SAI_STATUS_SUCCESS)
+
+        print("Waiting for switch to get ready, {} seconds ...".format(
+            switch_init_wait))
+        time.sleep(switch_init_wait)
+        return switch_id
---
+++ test/sai_test/constant.py
@@ -0,0 +1,29 @@
+# Copyright (c) 2021 Microsoft Open Technologies, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+#    THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR
+#    CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT
+#    LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS
+#    FOR A PARTICULAR PURPOSE, MERCHANTABILITY OR NON-INFRINGEMENT.
+#
+#    See the Apache Version 2.0 License for specific language governing
+#    permissions and limitations under the License.
+#
+#    Microsoft would like to thank the following companies for their review and
+#    assistance with these files: Intel Corporation, Mellanox Technologies Ltd,
+#    Dell Products, L.P., Facebook, Inc., Marvell International Ltd.
+#
+#
+
+DEFAULT_IP_V4_PREFIX = '0.0.0.0/0'
+DEFAULT_IP_V6_PREFIX = '0000:0000:0000:0000:0000:0000:0000:0000'
+FDB_MAC_PREFIX = '00:01:01'
+FDB_SERVER_NUM = '99'
+LOCAL_IP_128V6_PREFIX = 'fe80::f68e:38ff:fe16:bc75/128'
+LOCAL_IP_10V6_PREFIX = 'fe80::/10'
+PORT_MTU = 9122
+ROUTER_MAC = '00:77:66:55:44:00'
+THRIFT_PORT = 9092
