194ca29085ab41b5f5afcbf7708fa171695302d4
Jai Kumar <jai.kumar@broadcom.com>
2022-07-11 14:58:21 -0700

    Buffer Pool Family
    Signed-off-by: Jai Kumar <jai.kumar@broadcom.com>

--- inc/saitypes.h
+++ inc/saitypes.h
@@ -289,6 +289,8 @@ typedef enum _sai_object_type_t
     SAI_OBJECT_TYPE_IPSEC                    = 99,
     SAI_OBJECT_TYPE_IPSEC_PORT               = 100,
     SAI_OBJECT_TYPE_IPSEC_SA                 = 101,
+    SAI_OBJECT_TYPE_BUFFER_POOL_FAMILY       = 102,
+
     SAI_OBJECT_TYPE_MAX,  /* Must remain in last position */
 } sai_object_type_t;
 
