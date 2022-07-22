9978f19b582c5577baf4541fbcf68b83775fd1ae
richardyu-ms <richard.yu@microsoft.com>
2022-07-22 11:43:14 +0000

    Build saithrift with python3.9 in bullseye
    Bullseye upgrade the python to python3.9 and other image, like buster still using python 2.7.
    To build with different python in different image, add a condition for copy differernt install file in different type of image.
    
    Signed-off-by: richardyu-ms <richard.yu@microsoft.com>

--- debian/copy_installer.sh
+++ debian/copy_installer.sh
@@ -3,6 +3,12 @@ then
    echo "Copy python-saithriftv2.install as python-saithrift.install"
    cp ./debian/installerFiles/python-saithriftv2.install ./debian/python-saithrift.install
 else
-   echo "Copy python-saithriftv1.install as python-saithrift.install"
-   cp ./debian/installerFiles/python-saithriftv1.install ./debian/python-saithrift.install
+   if test -n "lsb_release -a | grep bullseye"
+   then
+      echo "Copy python-saithriftv1-bullseye.install as python-saithrift.install"
+      cp ./debian/installerFiles/python-saithriftv1-bullseye.install ./debian/python-saithrift.install
+   else
+      echo "Copy python-saithriftv1.install as python-saithrift.install"
+      cp ./debian/installerFiles/python-saithriftv1.install ./debian/python-saithrift.install
+   fi   
 fi
