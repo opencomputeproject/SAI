f234aa08f18e7d023054a6304e6b5d6451a6949f
zhiyuan-ms <t-zhliang@microsoft.com>
2022-09-05 07:56:49 +0000

    add ecmp test cases
    Signed-off-by: zhiyuan-ms <t-zhliang@microsoft.com>

--- test/sai_test/data_module/device.py
+++ test/sai_test/data_module/device.py
@@ -23,7 +23,7 @@ from constant import *
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from data_module.nexthop import Nexthop
-    from data_module.ecmp import Ecmp
+    from nexthop_group import NexthopGroup
     from data_module.lag import Lag
 
 
@@ -65,7 +65,7 @@ class Device(object):
             l2_egress_port_idx
             l3_port_idx
             l3_lag_obj
-            ecmp
+            nhp_grp
 
         Server:
             self.ip_pattern: SERVER_IPV4_PATTERN
@@ -130,9 +130,13 @@ class Device(object):
         """
         L3 destination lag object, defined in route configer
         """
-        self.ecmp : Ecmp = None
+        self.l3_nhp_grpv4 : NexthopGroup = None
         """
-        L3 destination ecmp object, defined in route configer
+        L3 destination nexthop group ipv4 object, defined in route configer
+        """
+        self.l3_nhp_grpv6 : NexthopGroup = None
+        """
+        L3 destination nexthop group ipv6 object, defined in route configer
         """
 
 
