e23b34c7c3ee8b3e20ab6be468e8500d739c96d9
rlhui <48738894+rlhui@users.noreply.github.com>
2022-10-16 22:35:12 -0700

    Update SAI version to v1.11 (#1623)
    Update SAI version to v1.11

--- inc/saiversion.h
+++ inc/saiversion.h
@@ -28,7 +28,7 @@
 #include <saitypes.h>
 
 #define SAI_MAJOR 1
-#define SAI_MINOR 10
+#define SAI_MINOR 11
 #define SAI_REVISION 0
 
 #define SAI_VERSION(major, minor, revision) (10000 * (major) + 100 * (minor) + (revision))
