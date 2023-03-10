63519d0e4ce76f92ae25cd4735fb86827b7e10a7
Jai Kumar <jai.kumar@broadcom.com>
2023-03-10 10:48:28 -0800

    Credit WD and VOQ delete counter
    Signed-off-by: Jai Kumar <jai.kumar@broadcom.com>

--- inc/saiswitch.h
+++ inc/saiswitch.h
@@ -2796,6 +2796,30 @@ typedef enum _sai_switch_attr_t
     SAI_SWITCH_ATTR_ACL_STAGE_POST_INGRESS,
 
     /**
+     * @brief Enable or disable credit watchdog
+     *
+     * Credit Watchdog can be enabled or disabled using this attribute for VOQ based system
+     *
+     * @type bool
+     * @flags CREATE_AND_SET
+     * @default true
+     * @validonly SAI_SWITCH_ATTR_TYPE == SAI_SWITCH_TYPE_VOQ
+     */
+    SAI_SWITCH_ATTR_CREDIT_WD,
+
+    /**
+     * @brief Credit watchdog threshold timer in milliseconds
+     * Value must be within 10ms - 1000ms range
+     * Queue is set to delete state and all packets in queue are deleted after the timer expiry
+     *
+     * @type sai_uint32_t
+     * @flags CREATE_AND_SET
+     * @default 500
+     * @validonly SAI_SWITCH_ATTR_TYPE == SAI_SWITCH_TYPE_VOQ
+     */
+    SAI_SWITCH_ATTR_CREDIT_WD_TIMER,
+
+    /**
      * @brief End of attributes
      */
     SAI_SWITCH_ATTR_END,
