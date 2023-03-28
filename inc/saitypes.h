2ac39379463169f8d9e00240f30d383ecf188165
Jai Kumar <jai.kumar@broadcom.com>
2023-03-28 14:35:18 -0700

    ACL Table Chain Group
    Signed-off-by: Jai Kumar <jai.kumar@broadcom.com>

--- inc/saitypes.h
+++ inc/saitypes.h
@@ -292,6 +292,7 @@ typedef enum _sai_object_type_t
     SAI_OBJECT_TYPE_GENERIC_PROGRAMMABLE     = 102,
     SAI_OBJECT_TYPE_ARS_PROFILE              = 103,
     SAI_OBJECT_TYPE_ARS                      = 104,
+    SAI_OBJECT_TYPE_ACL_TABLE_CHAIN_GROUP    = 105,
     SAI_OBJECT_TYPE_MAX,  /* Must remain in last position */
 } sai_object_type_t;
 
