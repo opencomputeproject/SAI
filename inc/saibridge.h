bbcf1c3fc9b7448287ac5ebaba65520b7bd4029b
Rida Hanif <61490193+ridahanif96@users.noreply.github.com>
2023-04-27 22:27:17 +0500

    Hybrid Bridge Port Tagging mode addition

--- inc/saibridge.h
+++ inc/saibridge.h
@@ -98,6 +98,11 @@ typedef enum _sai_bridge_port_tagging_mode_t
     /** Tagged mode */
     SAI_BRIDGE_PORT_TAGGING_MODE_TAGGED,
 
+
+    /** Hybrid mode */
+    SAI_BRIDGE_PORT_TAGGING_MODE_HYBRID,
+
+
 } sai_bridge_port_tagging_mode_t;
 
 /**
