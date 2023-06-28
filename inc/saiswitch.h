b874c18b9856e14fc57d7bfb552982e3fe293590
Jai Kumar <jai.kumar@broadcom.com>
2023-06-28 16:58:27 -0700

    VoQ Switch Isolation (#1793)
    Switch Isolate validonly condition addition for VoQ switch.
    
    Signed-off-by: Jai Kumar <jai.kumar@broadcom.com>

--- inc/saiswitch.h
+++ inc/saiswitch.h
@@ -2497,7 +2497,7 @@ typedef enum _sai_switch_attr_t
      *
      * @type sai_uint32_t
      * @flags MANDATORY_ON_CREATE | CREATE_ONLY
-     * @condition SAI_SWITCH_ATTR_TYPE == SAI_SWITCH_TYPE_VOQ
+     * @condition SAI_SWITCH_ATTR_TYPE == SAI_SWITCH_TYPE_VOQ or SAI_SWITCH_ATTR_TYPE == SAI_SWITCH_TYPE_FABRIC
      */
     SAI_SWITCH_ATTR_SWITCH_ID,
 
@@ -2851,13 +2851,17 @@ typedef enum _sai_switch_attr_t
      *
      * true: Trigger the switch isolate process
      * false: Undo the isolation operation.
-     * This attribute is for Fabric Chassis only
+     * This attribute is for Fabric or VOQ Chassis only
+     * Setting this flag to true will stop data traffic from flowing
+     * on local fabric ports. Fabric control traffic like credit grants
+     * and control messages will still continue to flow. Setting this
+     * flag to false will (re)enable data traffic on fabric ports.
      * If this attribute is set to true, it overrides port level isolation setting.
      *
      * @type bool
      * @flags CREATE_AND_SET
      * @default false
-     * @validonly SAI_SWITCH_ATTR_TYPE == SAI_SWITCH_TYPE_FABRIC
+     * @validonly SAI_SWITCH_ATTR_TYPE == SAI_SWITCH_TYPE_FABRIC or SAI_SWITCH_ATTR_TYPE == SAI_SWITCH_TYPE_VOQ
      */
     SAI_SWITCH_ATTR_SWITCH_ISOLATE,
 
