2cd20cc7305ca4a90a1730da4297ecfa51fc988a
Kamil Cudnik <kcudnik@gmail.com>
2023-07-06 18:55:30 +0200

    [meta] Add auto backward compatibility check for structs/unions (#1817)
    This will make sure that all structures and unions sizes will be always checked vs master branch, even if new items will be added in th future. No need for manual size check.

---
+++ meta/size.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# Copyright (c) 2023 Microsoft Open Technologies, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+#    THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR
+#    CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT
+#    LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS
+#    FOR A PARTICULAR PURPOSE, MERCHANTABILITY OR NON-INFRINGEMENT.
+#
+#    See the Apache Version 2.0 License for specific language governing
+#    permissions and limitations under the License.
+#
+#    Microsoft would like to thank the following companies for their review and
+#    assistance with these files: Intel Corporation, Mellanox Technologies Ltd,
+#    Dell Products, L.P., Facebook, Inc., Marvell International Ltd.
+#
+# @file    size.sh
+#
+# @brief   This module generates saimetadatasize.h with struct/union sizes
+#
+
+set -e
+
+TEMP_DIR="tmp"
+
+COMMIT=origin/master # should be corresponding branch HEAD
+
+rm -rf $TEMP_DIR
+
+mkdir $TEMP_DIR
+
+echo "git checkout dir inc and experimental on commit: $COMMIT"
+
+git --work-tree=$TEMP_DIR/ checkout $COMMIT inc experimental 2>/dev/null
+
+perl size.pl $TEMP_DIR
+
+rm -rf $TEMP_DIR
