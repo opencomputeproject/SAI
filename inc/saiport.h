60fb4ab787ccc69c9da2ea955fd95d2a9c1f8f77
Aparna Pattekar <aparnapattekar@gmail.com>
2023-08-09 10:40:31 -0700

    Proposal for SAI attributes for path tracing.
    Signed-off-by: Aparna Pattekar <aparnapattekar@gmail.com>

--- inc/saiport.h
+++ inc/saiport.h
@@ -490,6 +490,25 @@ typedef enum _sai_port_dual_media_t
 } sai_port_dual_media_t;
 
 /**
+ * @brief Attribute data for #SAI_PORT_ATTR_PATH_TRACING_TIMESTAMP_TYPE
+ */
+typedef enum _sai_port_path_tracing_timestamp_type_t
+{
+    /** Timestamp nanosecond bits [8:15] */
+    SAI_PORT_PATH_TRACING_TIMESTAMP_TYPE_8_15,
+
+    /** Timestamp nanosecond bits [12:19] */
+    SAI_PORT_PATH_TRACING_TIMESTAMP_TYPE_12_19,
+
+    /** Timestamp nanosecond bits [16:23] */
+    SAI_PORT_PATH_TRACING_TIMESTAMP_TYPE_16_23,
+
+    /** Timestamp nanosecond bits [20:27] */
+    SAI_PORT_PATH_TRACING_TIMESTAMP_TYPE_20_27,
+
+} sai_port_path_tracing_timestamp_type_t;
+
+/**
  * @brief Attribute Id in sai_set_port_attribute() and
  * sai_get_port_attribute() calls
  */
@@ -2283,6 +2302,28 @@ typedef enum _sai_port_attr_t
      * @flags READ_ONLY
      */
     SAI_PORT_ATTR_OPER_PORT_FEC_MODE,
+
+    /**
+     * @brief Configure 12b path tracing interface id
+     *
+     * Set interface id = 0 to disable path tracing on port.
+     * Valid values for interface id are 0 - 4095.
+     *
+     * @type sai_uint16_t
+     * @flags CREATE_AND_SET
+     * @isvlan false
+     * @default 0
+     */
+    SAI_PORT_ATTR_PATH_TRACING_INTF,
+
+    /**
+     * @brief Configure path tracing timestamp template
+     *
+     * @type sai_port_path_tracing_timestamp_type_t
+     * @flags CREATE_AND_SET
+     * @default SAI_PORT_PATH_TRACING_TIMESTAMP_TYPE_8_15
+     */
+    SAI_PORT_ATTR_PATH_TRACING_TIMESTAMP_TYPE,
 
     /**
      * @brief End of attributes
