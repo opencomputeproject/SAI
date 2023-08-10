5ace3724c5e2d376917caf4b2fb476a890d2b2ff
Manali Holankar <manali.holankar@gmail.com>
2023-08-10 10:13:30 -0700

    Add Priority for Tunnel Term Table Entries
    Since Tunnel Term Table supports ternary matching for Dest IP+Mask and/or SRC IP+Mask, added priority
    
    Signed-off-by: Manali Holankar <manali.holankar@gmail.com>

--- inc/saiswitch.h
+++ inc/saiswitch.h
@@ -2930,6 +2930,22 @@ typedef enum _sai_switch_attr_t
     SAI_SWITCH_ATTR_ACL_CHAIN_LIST,
 
     /**
+     * @brief Minimum priority for Tunnel Term table
+     *
+     * @type sai_uint32_t
+     * @flags READ_ONLY
+     */
+    SAI_SWITCH_ATTR_TUNNEL_TERM_TABLE_ENTRY_MINIMUM_PRIORITY,
+
+    /**
+     * @brief Maximum priority for Tunnel Term table
+     *
+     * @type sai_uint32_t
+     * @flags READ_ONLY
+     */
+    SAI_SWITCH_ATTR_TUNNEL_TERM_TABLE_ENTRY_MAXIMUM_PRIORITY,
+
+    /**
      * @brief End of attributes
      */
     SAI_SWITCH_ATTR_END,
