621e676b2837a1553a259cba14a529a861b4a396
Chikkegowda Chikkaiah <cchikkai@cisco.com>
2024-01-08 21:07:46 -0500

    SAI proposal for icmp echo offload
    Signed-off-by: Chikkegowda Chikkaiah <cchikkai@cisco.com>
    
    update doc
    
    Signed-off-by: Chikkegowda Chikkaiah <cchikkai@cisco.com>
    
    build fix
    
    Signed-off-by: Chikkegowda Chikkaiah <cchikkai@cisco.com>
    
    build fix
    
    Signed-off-by: Chikkegowda Chikkaiah <cchikkai@cisco.com>
    
    fixes
    
    Signed-off-by: Chikkegowda Chikkaiah <cchikkai@cisco.com>
    
    fixes
    
    Signed-off-by: Chikkegowda Chikkaiah <cchikkai@cisco.com>
    
    fixes
    
    Signed-off-by: Chikkegowda Chikkaiah <cchikkai@cisco.com>
    
    fixes
    
    Signed-off-by: Chikkegowda Chikkaiah <cchikkai@cisco.com>
    
    doc fix
    
    Signed-off-by: Chikkegowda Chikkaiah <cchikkai@cisco.com>
    
    build fix
    
    Signed-off-by: Chikkegowda Chikkaiah <cchikkai@cisco.com>
    
    remove whitespaces
    
    Signed-off-by: Chikkegowda Chikkaiah <cchikkai@cisco.com>
    
    build fix
    
    Signed-off-by: Chikkegowda Chikkaiah <cchikkai@cisco.com>
    
    build fix
    
    Signed-off-by: Chikkegowda Chikkaiah <cchikkai@cisco.com>
    
    lookup valid default
    
    Signed-off-by: Chikkegowda Chikkaiah <cchikkai@cisco.com>
    
    build fixes
    
    Signed-off-by: Chikkegowda Chikkaiah <cchikkai@cisco.com>
    
    build fix
    
    Signed-off-by: Chikkegowda Chikkaiah <cchikkai@cisco.com>
    
    doc changes
    
    Signed-off-by: Chikkegowda Chikkaiah <cchikkai@cisco.com>
    
    fix sequence number size in doc
    
    Signed-off-by: Chikkegowda Chikkaiah <cchikkai@cisco.com>
    
    add attribute to enable protection group switchover
    
    Signed-off-by: Chikkegowda Chikkaiah <cchikkai@cisco.com>
    
    doc fix
    
    Signed-off-by: Chikkegowda Chikkaiah <cchikkai@cisco.com>
    
    doc fix
    
    Signed-off-by: Chikkegowda Chikkaiah <cchikkai@cisco.com>
    
    doc fix
    
    Signed-off-by: Chikkegowda Chikkaiah <cchikkai@cisco.com>
    
    moving APIs to extenstions
    
    Signed-off-by: Chikkegowda Chikkaiah <cchikkai@cisco.com>
    
    fixes
    
    Signed-off-by: Chikkegowda Chikkaiah <cchikkai@cisco.com>
    
    fixes
    
    Signed-off-by: Chikkegowda Chikkaiah <cchikkai@cisco.com>
    
    fixes
    
    Signed-off-by: Chikkegowda Chikkaiah <cchikkai@cisco.com>
    
    fixes
    
    Signed-off-by: Chikkegowda Chikkaiah <cchikkai@cisco.com>

--- experimental/saiswitchextensions.h
+++ experimental/saiswitchextensions.h
@@ -37,6 +37,17 @@ typedef enum _sai_switch_attr_extensions_t
 {
     SAI_SWITCH_ATTR_EXTENSIONS_RANGE_START = SAI_SWITCH_ATTR_END,
 
+    /**
+     * @brief Set Switch ICMP_ECHO session state change event notification callback function passed to the adapter.
+     *
+     * Use sai_icmp_echo_session_state_change_notification_fn as notification function.
+     *
+     * @type sai_pointer_t sai_icmp_echo_session_state_change_notification_fn
+     * @flags CREATE_AND_SET
+     * @default NULL
+     */
+    SAI_SWITCH_ATTR_ICMP_ECHO_SESSION_STATE_CHANGE_NOTIFY,
+
     SAI_SWITCH_ATTR_EXTENSIONS_RANGE_END
 
 } sai_switch_attr_extensions_t;
