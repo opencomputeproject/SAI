f7389badc62f1fdf8138a2ca04c76762aee6021b
Kamil Cudnik <kcudnik@gmail.com>
2024-03-01 13:55:15 +0100

    Remove depreacated bm directory (#1954)
    As planned as of end of 2023 this directory is being depreacated

--- bm/p4-sai/run_server.sh
+++
@@ -1,9 +0,0 @@
-set -m
-rm -rf log.txt
-sudo -v
-# sudo ./simple_switch -i 0@sw_port0 -i 1@sw_port1 -i 2@sw_port2 -i 3@sw_port3 -i 4@sw_port4 -i 5@sw_port5 -i 6@sw_port6 -i 7@sw_port7 -i 8@cpu_port --log-file log --log-flush sai.json &
-sudo ./simple_switch -i 0@sw_port0 -i 1@sw_port1 -i 2@sw_port2 -i 3@sw_port3 -i 4@sw_port4 -i 5@sw_port5 -i 6@sw_port6 -i 7@sw_port7 -i 8@cpu_port --log-file log --log-flush --pcap sai.json &
-sleep 2
-./runtime_CLI --pre SimplePreLAG < p4src/DefaultConfig.txt
-./sswitch_CLI < p4src/DefaultConfigMirror.txt
-fg
--- bm/sai_adapter/test/ptf_tests/run_ptf_c.sh
+++
@@ -1,4 +0,0 @@
-sudo ./ptf/ptf --test-dir tests/ --pypath sai_thrift_src/gen-py --test-params port_map_file=\"port_map.ini\" --interface 0@sw_port0 --interface 1@sw_port1 --interface 2@sw_port2 \
-    	 --interface 3@sw_port3 --interface 4@sw_port4 --interface 5@sw_port5 --interface 6@sw_port6 --interface 7@sw_port7  \
-    	 sail2_new.L2AcceptedFrameType sail2_new.L21DBridgeBasicTest sail2_new.L21QBridgeAccess2AccessTest sail2_new.L21QBridgeAccess2TrunkTest sail2_new.L21QBridgeTrunk2AccessTest sail2_new.L21QBridgeTrunk2TrunkTest sail2_new.L21DLagTest
-    	 # sail2_new.L21QLagTest
