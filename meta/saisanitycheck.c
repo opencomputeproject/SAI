ae280e095b9e7ef5ec68765523d3e53ba42e1ac3
Riff <r12f.code@gmail.com>
2024-04-24 09:22:17 -0700

    Add sai_u32_range_t support for SAI struct entries. (#2002)
    Add sai_u32_range_t support for SAI struct entries.
    
    Signed-off-by: r12f <r12f.code@gmail.com>

--- meta/saisanitycheck.c
+++ meta/saisanitycheck.c
@@ -3657,6 +3657,7 @@ void check_non_object_id_object_types()
                 case SAI_ATTR_VALUE_TYPE_UINT32:
                 case SAI_ATTR_VALUE_TYPE_UINT16:
                 case SAI_ATTR_VALUE_TYPE_UINT8:
+                case SAI_ATTR_VALUE_TYPE_UINT32_RANGE:
                 case SAI_ATTR_VALUE_TYPE_IP_ADDRESS:
                 case SAI_ATTR_VALUE_TYPE_IP_PREFIX:
                 case SAI_ATTR_VALUE_TYPE_OBJECT_ID:
