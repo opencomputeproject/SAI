a0ae84d9366d35bfc50738c3b29e03387bf85c7c
Mukesh Moopath Velayudhan <mukesh@pensando.io>
2024-09-04 17:07:05 -0700

    Add DASH appliance object APIs (#2073)
    Add DASH appliance object APIs (#2073)
    Bring in SAI API changes from https://github.com/sonic-net/DASH/pull/616

--- experimental/saiextensions.h
+++ experimental/saiextensions.h
@@ -34,6 +34,7 @@
 #include "saiportextensions.h"
 
 /* new experimental object type includes */
+#include "saiexperimentaldashappliance.h"
 #include "saiexperimentaldashflow.h"
 #include "saiexperimentaldashtunnel.h"
 #include "saiexperimentaldashha.h"
@@ -85,6 +86,9 @@ typedef enum _sai_api_extensions_t
     SAI_API_DASH_TUNNEL,
 
     SAI_API_DASH_FLOW,
+
+    SAI_API_DASH_APPLIANCE,
+
     /* Add new experimental APIs above this line */
 
     SAI_API_EXTENSIONS_RANGE_END
