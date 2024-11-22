f6ff7e636f26a9e31f7ace7f87c27595e8d63a42
kcudnik <kcudnik@gmail.com>
2024-11-22 14:21:55 +0100

    [meta] Update history files
    For faster validation

--- meta/checkancestry.sh
+++ meta/checkancestry.sh
@@ -106,7 +106,7 @@ function create_commit_list()
 
 function check_enum_history()
 {
-    perl ancestry.pl -H "ancestry.825c835.history" $LIST
+    perl ancestry.pl -H "ancestry.5f75d99.history" $LIST
 }
 
 #
@@ -122,7 +122,7 @@ function check_enum_history()
 # the processing time will increase too much
 
 BEGIN_COMMIT=3132018 # from this commit we are backward compatible
-BEGIN_COMMIT=825c835 # to this commit we have history file
+BEGIN_COMMIT=5f75d99 # to this commit we have history file
 END_COMMIT=HEAD
 
 clean_temp_dir
