9acebac6cf6934542e908d28681283e5decd8e45
Rich Sugarman <rsugarman@nvidia.com>
2025-03-06 14:54:06 +0000

    Move new enum above its comment reference for doc generation
    Signed-off-by: Rich Sugarman <rsugarman@nvidia.com>

--- inc/saiswitch.h
+++ inc/saiswitch.h
@@ -634,6 +634,22 @@ typedef enum _sai_packet_trim_queue_resolution_mode_t
 
 } sai_packet_trim_queue_resolution_mode_t;
 
+/** 
+ * @brief Global PTP configuration
+ */
+typedef struct _sai_switch_ptp_mode_t
+{
+    /** None - per-port configuration can be used to switch PTP on for a port */
+    SAI_SWITCH_PTP_MODE_NONE,
+
+    /** One-step - all ports use one-step PTP mode */
+    SAI_SWITCH_PTP_MODE_SINGLE_STEP_TIMESTAMP,
+
+    /** Two-step - all ports use two-step PTP mode */
+    SAI_SWITCH_PTP_MODE_TWO_STEP_TIMESTAMP
+    
+} sai_switch_ptp_mode_t;
+
 /**
  * @brief Attribute Id in sai_set_switch_attribute() and
  * sai_get_switch_attribute() calls.
@@ -3240,22 +3256,6 @@ typedef enum _sai_switch_asic_sdk_health_severity_t
     SAI_SWITCH_ASIC_SDK_HEALTH_SEVERITY_NOTICE
 
 } sai_switch_asic_sdk_health_severity_t;
-
-/** 
- * @brief Global PTP configuration
- */
-typedef struct _sai_switch_ptp_mode_t
-{
-    /** None - per-port configuration can be used to switch PTP on for a port */
-    SAI_SWITCH_PTP_MODE_NONE,
-
-    /** One-step - all ports use one-step PTP mode */
-    SAI_SWITCH_PTP_MODE_SINGLE_STEP_TIMESTAMP,
-
-    /** Two-step - all ports use two-step PTP mode */
-    SAI_SWITCH_PTP_MODE_TWO_STEP_TIMESTAMP
-    
-} sai_switch_ptp_mode_t;
 
 /**
  * @brief Switch health categories
