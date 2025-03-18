eeac27b60fecac0284ca02167b8a66f345fb7c2f
Prince George <prgeor@microsoft.com>
2025-03-18 02:53:23 +0000

    Add SAI port attribute to fetch PAM4 eye heights
    Signed-off-by: Prince George <prgeor@microsoft.com>

--- meta/saimetadatatypes.h
+++ meta/saimetadatatypes.h
@@ -495,6 +495,11 @@ typedef enum _sai_attr_value_type_t
      * @brief Attribute value is the POE port consumption data.
      */
     SAI_ATTR_VALUE_TYPE_POE_PORT_POWER_CONSUMPTION,
+
+    /**
+     * @brief Attribute value is port PAM4 eye values list.
+     */
+    SAI_ATTR_VALUE_TYPE_PORT_PAM4_EYE_VALUES_LIST,
 } sai_attr_value_type_t;
 
 /**
