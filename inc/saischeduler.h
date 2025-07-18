9ce60192b7d5abdbcea5fdd66f07caa5cf9888ec
Kumaresh Perumal <kperumal@microsoft.com>
2025-07-18 21:07:11 +0000

    Update comment with invalid weight value
    Signed-off-by: Kumaresh Perumal <kperumal@microsoft.com>

--- inc/saischeduler.h
+++ inc/saischeduler.h
@@ -71,7 +71,7 @@ typedef enum _sai_scheduler_attr_t
     /**
      * @brief Scheduling algorithm weight
      *
-     * Range [1 - 255].
+     * Range [0 - 255], 0-invalid weight
      *
      * @type sai_uint8_t
      * @flags CREATE_AND_SET
