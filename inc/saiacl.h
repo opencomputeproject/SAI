451baf5d294760d0471a9de9b93d2d00a60a56dd
JaiOCP <jai.kumar@broadcom.com>
2025-08-13 13:08:57 -0700

    CSIG Support
    Signed-off-by: JaiOCP <jai.kumar@broadcom.com>

--- inc/saiacl.h
+++ inc/saiacl.h
@@ -1580,9 +1580,18 @@ typedef enum _sai_acl_table_attr_t
     SAI_ACL_TABLE_ATTR_FIELD_DST_PREFIX_META = SAI_ACL_TABLE_ATTR_FIELD_START + 0x161,
 
     /**
+     * @brief CSIG TRIM Bit D
+     *
+     * @type bool
+     * @flags CREATE_ONLY
+     * @default false
+     */
+    SAI_ACL_TABLE_ATTR_FIELD_TRIM_BIT = SAI_ACL_TABLE_ATTR_FIELD_START + 0x162,
+
+    /**
      * @brief End of ACL Table Match Field
      */
-    SAI_ACL_TABLE_ATTR_FIELD_END = SAI_ACL_TABLE_ATTR_FIELD_DST_PREFIX_META,
+    SAI_ACL_TABLE_ATTR_FIELD_END = SAI_ACL_TABLE_ATTR_FIELD_TRIM_BIT,
 
     /**
      * @brief ACL table entries associated with this table.
@@ -2708,9 +2717,18 @@ typedef enum _sai_acl_entry_attr_t
     SAI_ACL_ENTRY_ATTR_FIELD_DST_PREFIX_META = SAI_ACL_ENTRY_ATTR_FIELD_START + 0x161,
 
     /**
+     * @brief CSIG TRIM Bit D
+     *
+     * @type sai_acl_field_data_t bool
+     * @flags CREATE_AND_SET
+     * @default disabled
+     */
+    SAI_ACL_ENTRY_ATTR_FIELD_TRIM_BIT = SAI_ACL_ENTRY_ATTR_FIELD_START + 0x162,
+
+    /**
      * @brief End of Rule Match Fields
      */
-    SAI_ACL_ENTRY_ATTR_FIELD_END = SAI_ACL_ENTRY_ATTR_FIELD_DST_PREFIX_META,
+    SAI_ACL_ENTRY_ATTR_FIELD_END = SAI_ACL_ENTRY_ATTR_FIELD_TRIM_BIT,
 
     /*
      * Actions [sai_acl_action_data_t]
